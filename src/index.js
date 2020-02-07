148cbab1d50de448c0a655739db331ce83185315
Alissang <alitasang@gmail.com>
2020-02-07 13:17:48 -0600

    hola

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function secondPage () {
+document.getElementById("inicio").style.display = "none";
+document.getElementById("instruccions").style.display = "block";
+};
+
+
