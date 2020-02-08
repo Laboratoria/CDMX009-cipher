d06689325d0e1dfa34f981c180b348eafe8d3b5b
Sheiki1 <kirokomerteabesos@gmail.com>
2020-02-08 17:36:30 -0600

    voy avanzando

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+    function cifrar() {
+    let cipherText = document.getElementById("cipherText");
+    let texto = cipherText.value;
+                 
+    let cipherKey = document.getElementById("cipherKey");
+    let numero = cipherKey.value;
+
+    let caracteres = [];
+    let formula=[];
+    let concatenarCaracteres="";
+    let charnum=0;
+    let varnumber=0;
+  
+    for (let i = 0; i < texto.length; i++){
+                                            charnum = texto.charAt(i).charCodeAt(0);
+                                            varnumber = parseInt(numero);
+                                            charnum = charnum + varnumber;
+                                            caracteres[i]=String.fromCharCode(charnum);
+                                            concatenarCaracteres += caracteres[i]+"";
+                                        }          
+    let element = document.getElementById("resultado");
+    element.innerHTML = concatenarCaracteres.toString();
+}
+
+let limpiar = document.getElementById("limpiar");
