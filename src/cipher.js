aacf6158536ea842543814bfabade7f5f22e5b12
EdithOrt <eoc.diseno@gmail.com>
2020-02-10 13:08:19 -0600

    comenzando javascript de cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,17 @@
-let numeroCifrado= 
-function cifrar() {
-    
+let mensaje= document.getElementById ("cajaMensaje").value;
+let desplazamiento= document.getElementById("cajaClave").value;
+
+
+if cifrar() {
+(mensaje.length=" "){
+    document.getElementById("advertencia2").innerText= "Ingresar mensaje."
+ }
+    else if (mensaje==50.length){ 
+        (mensaje.charcodeAt -65 + desplazamiento)%26+65;
+        document.getElementById("pantallaUno").style.display="none";
+        document.getElementById("pantallaDos").style.display="block";
+
+ }
+ console.log(cifrar)
 }
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 function recogeDatos() {
 
-let nombre= document.getElementById("cajanombre").value;
-
-if (nombre.length == " "){
-    document.getElementById("advertencia").innerText = "Por favor poner tu nombre";
-} else {
-    document.getElementById("pantallaUno").style.display="none";
-    document.getElementById("pantallaDos").style.display="block";
-    document.getElementById("bienvenido").innerText= "Bienvenid@ " + nombre;
-}
-}
+    let nombre= document.getElementById("cajanombre").value;
+    
+    if (nombre.length == " "){
+        document.getElementById("advertencia").innerText = "Por favor poner tu nombre";
+    } else {
+        document.getElementById("pantallaUno").style.display="none";
+        document.getElementById("pantallaDos").style.display="block";
+        document.getElementById("bienvenido").innerText= "Bienvenid@ " + nombre;
+    }
+    }
