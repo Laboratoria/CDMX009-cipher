dc4b3b4dc5bc7b8f43cc7d9abcbf3013c037daaa
Sheiki1 <kirokomerteabesos@gmail.com>
2020-02-10 12:24:04 -0600

    termine diseño

--- src/index.js
+++ src/index.js
@@ -7,18 +7,25 @@
     let numero = cipherKey.value;
 
     let caracteres = [];
-    let formula=[];
-    let concatenarCaracteres=""
-    let charnum=0
-    let varnumber=0
+    
+    let concatenarCaracteres="";
+    let charnum=0;
+    let varnumber=0;
   
     for (let i = 0; i < texto.length; i++){
-                                            charnum = texto.charAt(i).charCodeAt(0)
-                                            varnumber = parseInt(numero)
-                                            charnum = (charnum -65 + varnumber)%26+65
-                                            caracteres[i]=String.fromCharCode(charnum)
-                                            concatenarCaracteres += caracteres[i]+""
-                                        }          
+        charnum = texto.charAt(i).charCodeAt(0);
+        varnumber = parseInt(numero);
+        if(65<=charnum<=90){
+            charnum = (charnum -65 + varnumber)%26+65;
+
+        }else if (97<=charnum<=122) {
+            charnum=(charnum-97+ varnumber)%26+97;
+        } else {
+            charnum=charnum;
+        }
+        caracteres[i]=String.fromCharCode(charnum);
+        concatenarCaracteres += caracteres[i]+"";
+    }          
     let element = document.getElementById("resultado");
     element.innerHTML=concatenarCaracteres.toString();
 }
@@ -29,22 +36,29 @@ function descifrar() {
     let numero = cipherKey.value;
     let texto = cipherText.value;
     let caracteres = [];
-    let formula=[];
     let charnum=0;
     let varnumber=0;
     let concatenarCaracteres="";
     
     for (let i = 0; i < texto.length; i++){
-                                            charnum=       texto.charAt(i).charCodeAt(0);
-                                            varnumber=parseInt(numero)
-                                            charnum = (charnum + 65 - varnumber)%26+65;
-                                            caracteres[i]=String.fromCharCode(charnum)
-                                            concatenarCaracteres += caracteres[i]+"";
-                                        }
+        charnum= texto.charAt(i).charCodeAt(0);
+        varnumber=parseInt(numero)
+        if(65<=charnum<=90){
+            charnum = (charnum +65 - varnumber)%26+65;
+        }else if (97<=charnum<=122) {
+            charnum=(charnum+97- varnumber)%26+97;
+        } else {
+            charnum=charnum;
+        }
+        caracteres[i]=String.fromCharCode(charnum);
+        concatenarCaracteres += caracteres[i]+"";
+    }
                   
     let element = document.getElementById("resultado");
     element.innerHTML = concatenarCaracteres.toString();
     
 }
 
-let limpiar = document.getElementById("limpiar");
+function limpiar() {
+    document.getElementById("resultado").innerText="";
+  }
