01560485c77b7a29bfa7f9a2a7d449591f3ced97
andreatoledo <andytoledo.e03@gmail.com>
2020-02-10 12:31:14 -0600

    cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
 import cipher from './cipher.js';
 
 console.log(cipher);
+//audio /document.getElementById("audio").value;
+//function myFunction() {
+//var x = document.getElementById("audio").autoplay;
+//document.getElementById("demo").innerHTML = x;
+//}
+
+let containerResult = document.getElementById('container');
+  
+document.getElementById("btnCifrar").addEventListener("click", () => {
+//console.log("hola");
+  let toEncode= document.getElementById("message").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  containerResult.innerHTML = cipher.encode(offset,toEncode);
+
+document.getElementById("btnDescifrar").addEventListener("click", () => {
+  let toDecode = document.getElementById("message").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  containerResult.innerHTML = cipher.decode(offset,toDecode);
+//console.log("hola");
+});
+document.getElementById("resetbtn").addEventListener("click", () => {
+  document.getElementById("message").value = "";
+  document.getElementById("offset").value = "";
+  document.getElementById("container").value = "";
+
+});
+});
