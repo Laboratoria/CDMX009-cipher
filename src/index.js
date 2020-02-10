203318b13e166810e164a66b8ca5902da9bd403c
AniaCuapio <ania.cuapio@gmail.com>
2020-02-10 14:56:53 -0600

    Avance de CSS y funcionalidad de los botonoes en JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+var str= document.getElementById(inputString).value;
+var offset= document.getElementById(offset);
+
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function segundapantalla(){
+  var pantallaMostrada= document.getElementById("pantallaUno");
+  pantallaMostrada.style= "display:none";
+  document.getElementById("pantallaDos").style.display="block";
+};
+
+function tercerapantalla(){
+  pantallaMostrada= document.getElementById("pantallaUno");
+  pantallaMostrada.style= "display:none";
+  document.getElementById("pantallaTres").style.display="block";
+};
+
+function pantallaUno(){
+    pantallaMostrada=document.getElementById("pantallaTres");
+    pantallaMostrada.style="display:none";
+    document.getElementById("pantallaUno").style.display="block";
+};
+
+document.getElementById("Cifrar").addEventListener("click", cipher.encode(str, offset));
+;
+document.getElementById("Descifrar").addEventListener("click", cipher.decode(str, offset));
+document.getElementById("Entrar").addEventListener("click", segundapantalla);
+document.getElementById("Volver").addEventListener("click", primerapantalla);
