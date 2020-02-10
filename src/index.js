484d4ffe2830d40c34d48a9a8dddef7cd2598821
Alissang <alitasang@gmail.com>
2020-02-09 22:29:38 -0600

    html final

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
-
-function secondPage () {
-document.getElementById("inicio").style.display = "none";
-document.getElementById("instruccions").style.display = "block";
-};
-
-
