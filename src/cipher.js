1ed7429a50432e1b49b83559000d06d797d6a3ad
Gaby Alvarez <gabyalvarzb@gamil.com>
2020-02-09 18:43:49 -0600

    creacion de funciones para codificar y decodificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+    encode: function(offset, textOrigen) {
+        var newCadena = "";
+        for (let index = 0; index < textOrigen.length; index++) {
+            var caracter = textOrigen.charAt(index);
+            var caracterASCII = caracter.charCodeAt(0);
+            var newCaracterASCII = ((caracterASCII - 65 + offset) % 26 + 65);
+            newCadena += String.fromCharCode(newCaracterASCII);
+        }
+
+        return newCadena;
+    },
+    decode: function(offset, textOrigen) {
+        var newCadena = "";
+        for (let index = 0; index < textOrigen.length; index++) {
+            var caracter = textOrigen.charAt(index);
+            var caracterASCII = caracter.charCodeAt(0);
+            var newCaracterASCII = ((caracterASCII - 65 - offset) % 26 + 65);
+            newCadena += String.fromCharCode(newCaracterASCII);
+        }
+
+        return newCadena;
+    }
 };
 
 export default cipher;
