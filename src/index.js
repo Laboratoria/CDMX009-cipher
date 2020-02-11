fd9c69a8ab8ba83c440b0e8c5658de40ee1caccd
IrisFyD <iriis.trejo.c@gmail.com>
2020-02-11 09:44:48 -0600

    Lenado de codigo html y css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+let name;
+  name= prompt("Estas a punto de entrar a una pagina súper secreta Ingresa tu alias");
+  document.write(name);
