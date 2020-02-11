00d0ff144077eb0f46cd3542690a8030ed96fbae
unknown <laura-a-c@hotmail.com>
2020-02-11 11:47:11 -0600

    agregre estilos y unpoco de javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function getName() {
+    let name=document .getElementById ("name").value;
+    let pat=document.getElementById ("pat").value;
+    let mat= document.getElementById ("mat").value;
+    console.log (name+ "" +pat+ "" +mat); 
+}
+
+//listener u observador
+document.querySelector('#lac').addEventListener('click', getName)
+
