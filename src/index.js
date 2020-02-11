ebe10e648c45d37b85fa25af2508b19c7f2fda02
veroalrodriguez <veroalrodriguez@gmail>
2020-02-11 10:13:26 -0600

    Modifiqué html y pruebas en js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 //import cipher from './cipher.js';
 
 //console.log(cipher);
+
+    //Validando folio
+    let folio = document.getElementById("folio").value;
+        if (folio = H0G2NB){
+          return true}else{
+            alert("Folio incorrecto")
+          }
+    send.addEventListener("click", function(){
+      document.getElementById("login").style.display = 'none'
+    	document.getElementById("first-page").style.display = 'block'
+
+    })
+
+
+
+
+
+
+window.onload = () => {
+    /*Enlaces*/
+    const linkActive = document.getElementById("linkEncode");
+    const linkDesactive = document.getElementById("linkDecode");
+
+}
