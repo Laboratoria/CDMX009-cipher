d2a8bfda82445668f2cd8d51540f73a03580adbb
Nallely <nallely232@hotmail.com>
2020-02-11 13:03:06 -0600

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-function inicio (){
+function showAndHide (){
   console.log("entre");
-  let screen = document.getElementById('quiz_wrapper');
+  let screen = document.getElementById("quiz_wrapper");
+  screen.style = "display:none";
+  document.getElementById('quiz_wrapper2').style.display ="block";
+
+
+}
+document.getElementById("boton").onclick = function() {showAndHide()};
+
+
+function showAndHide1 (){
+  console.log("entre");
+  let screen = document.getElementById("quiz_wrapper");
   screen.style = "display:none";
   document.getElementById('quiz_wrapper1').style.display ="block";
-    document.getElementById('quiz_wrapper2').style.display ="block";
+
+
 }
+document.getElementById("boton1").onclick = function() {showAndHide1()};
