fb4a802b937aa52146dbe63eb19b732fb3d8373f
keupa <kapirupia@gmail.com>
2020-02-11 10:08:00 -0600

    :)

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,9 @@
-const cipher = {
-  // ...
-};
 
+const cipher = {
+var nText = document.getElementById('nTxt');
+
+}
+
+};
+// [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
 export default cipher;
