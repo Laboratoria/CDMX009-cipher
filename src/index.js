89e1eb456ecb797d0a3e269787273b863c52814f
JimenaMV <jimenamanzanares.v@gmail.com>
2020-02-11 13:17:34 -0600

    primer cambio, maqueta html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function segundapantalla() {
+    let pantallas = document.getElementById("first-screen");
+    pantallas.style = "display:none";
+    document.getElementById("second-screen").style.display = "block";
+}
+
+document.getElementById("change").addEventListener("click", segundapantalla)
