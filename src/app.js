d946510afa3e5b56ed87f2a7775c15fa8ba03d46
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-02-11 09:55:17 -0600

    Se hicieron las secciones en html y se les dio estilo en css asi como su separación en div y ocultando con css. Se hizo un archivo app.js para pruebas

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 function testTwo(){
   document.getElementById('test').innerHTML = '¡Hola Mundo!';
 }
+
+let cipheredUser = 'AGUANTÁ';
+document.getElementById("ciphered").value = cipheredUser;
+
+let decipheredKeyword = 'TODAVIA NO SIRVE';
+document.getElementById("deciphered").value = decipheredKeyword;
