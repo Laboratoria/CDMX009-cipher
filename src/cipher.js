407321416fc5c75892eb1e514d3e1b31e33f51d0
LizethRivera04 <60666768+LizethRivera04@users.noreply.github.com>
2020-02-11 12:48:08 -0600

    Se agregaron algunas modificaciones a las vistas y un poco de JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
 const cipher = {
   // ...
-};
+  
 
-export default cipher;
+};
+let offset = 0
+
+
+
+function code(string,offset){
+  let letterCode = string.charCodeAt(0)
+  let newCode = letterCode+offset
+  let newLetter = String.fromCharCode(newCode)
+  // deberia colocarlo en una nueva palabra
+
+}
+
+
+
+//descifrar = String.fromCharCode(cifrar)
+
+
+
+//export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
-console.log(cipher);
+function saveNumber(){
+  offset = parseInt(document.getElementById('posiciones').value); //localStorage
+  alert("Hola " + offset);
+
+
+}
+
+function cifrar(){
+  var string = document.getElementById('cadenacif').value;
+
+  //let newString = code(string,offset)
+
+
+  string = string.toUpperCase();
+  var posAscii = string.charCodeAt(0);
+  
+  //var formula = (posAscii - 65 + offset) %26 + 65;
+  document.getElementById('resultadocif').innerHTML = posAscii;
+
+}
+//console.log(cipher);
+
+
+
+  
