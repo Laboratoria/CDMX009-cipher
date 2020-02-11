8e984f1b250065c57387035cfa538f80b7ab443f
Yeraldin Casas <60666819+YeraldinCasas@users.noreply.github.com>
2020-02-11 10:07:26 -0600

    Se trabajó html y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,18 @@
-const cipher = {
-  // ...
+const cipher = { cipher.encode
+(offset, string)
 };
-
 export default cipher;
+
+
+function cipher(string) = {
+    let string= " "
+    for = (let i= 0; i < string.length; i++) {
+    let numberLetter = string.charCodeAt (i);
+    let cipherFormula; 
+    let newLetter; 
+    if (numberLetter >= 65 && numberLetter <= 90);
+    cipherFormula = ( numberLetter - 65 + 33) % 26 + 65;
+    theNewLetter =  String.fromCharCode(cipherFormula);
+    }
+
+}
