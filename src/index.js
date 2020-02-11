61174c5c4a0695ddedcbf3f13cd53cd2c3b07b89
lysisbe <lybgs87@yahoo.com.mx>
2020-02-11 13:14:17 -0600

    HTML cambios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 
-import cipher from './cipher.js';
 
-console.log(cipher);
+function segundapantalla(){
+  let pantallas = document.getElementById("página-de-inicio");
+  pantallas.style = "display:none";
+  document.getElementById("ingresar-datos").style.display = "block";
+  document.getElementById("Next").addEventListener("click",segundapantalla);
+
+}
