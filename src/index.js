66e939e7b7783f4fde7d4502e27a45f8282dc5fc
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-02-11 13:14:57 -0600

    quedo listo la estructura principal de html y css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-function secondPage() {
+/*función que sirve para cambiar pantallas*/
+
+function secondPage() { /* nombre así a la función desde html (específico button onclick)*/
     let pantallas = document.getElementById("firstPage")
     pantallas.style = "display:none";
     document.getElementById("secondPage").style.display = "block";
+
   }
-  
+  /*Función que sirve para cambiar de color la pantalla*/
+
+function changeBackground(color) {/*nombré así a la función desde html mas específico desde el button con atributo title*/
+   document.body.style.background = color;
+}
+
+window.addEventListener("click",function() { changeBackground('#BF52FF') }); 
+/*se ejecuta la función al hacer click*/
