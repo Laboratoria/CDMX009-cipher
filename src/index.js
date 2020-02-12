dbfb49fe8e881399239d9ecae9222346ee86031e
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2020-02-12 12:52:50 -0600

    inicio js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
+//
+//console.log(cipher);//no mover 
 
-console.log(cipher);
+//CAMBIO DE PANTALLAS 
+//pantalla 1
+
+function pantalla2() {
+	
+document.getElementById("section1").style.display = "none";
+document.getElementById("section2").style.display = "block";
+}
+
+document.getElementById("boton1").onclick = function () {pantalla2(); };
+
+
+function pantalla3(){
+	document.getElementById("section1").style.display = "none";
+	document.getElementById("section3").style.display = "block";
+}
+
+document.getElementById("boton2").onclick = function() {pantalla3(); };
+
+//volver pantalla 2
+
+function volver1
