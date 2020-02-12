c4dc05af58ee94f7f7675c3d42eb338dc6ebfdb5
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-02-12 08:53:52 -0600

    Realicé prueba de cifrado para que me saliera en consola en cipher.js, le di funcionalidad a los botones en index.js, agregué pocos cambios al Readme.md aún me falta acabar el readme.md

--- src/cipher.js
+++ src/cipher.js
@@ -7,24 +7,31 @@ const cipher = {
 export default cipher;
 
 */
-{
-    var output; //se crea una variable de salida de resultado
-    for (let i = 0; i < str.length; i++) { // Recorriendo cada posición de la frase ingresada.
-        let c = str[i]; // Se obtiene un array con cada posición de la frase.
-        let patron = /^[a-zA-Z\s]*$/;
-        if (!c.search(patron)) { // Se comprueba que el nuevo array solo contenga letras.
-            let code = str.charCodeAt(i); // Se obtiene el número en dotación ASCII.
-            // Se compara para aplicar fórmula de mayúsculas o minúsculas
-            if ((code >= 65) && (code <= 90)) {
-                c = String.fromCharCode(((code - 65 + 33) % 26) + 65);
-            } else if ((code >= 97) && (code <= 122)) {
-                c = String.fromCharCode(((code - 97 + 33) % 26) + 97);
-            } else {
-                return 'Ingrese solo texto';
-            }
-            output += c;
+
+let string = "COMPRAR BOLETOS"; //luego cambiar a textarea
+
+let offset = 5; //luego cambiar a select
+function cifrar() {
+    let letras = string.split("");
+    // EL SPLIT es un metodo por lo que siempre va a llevar paréntesis: split()
+    // divide un String en un array.
+    // me devuelve un array.
+    //en cada uno de los elementos que encuentre lo que hace es cortar.
+
+    let resultado = "";
+    letras.forEach(function(letra) { //forEach lo uso para recorrer cada uno de mis elemntos del array letras.
+        codigo = letra.charCodeAt(); //en esta parte creé una variable de nombre código y uso el charCodeAt para convertir mis letras a su código ascii
+        console.log(codigo); //comprobando
+        if ((codigo >= 65) && (codigo <= 90)) { //aquí coloco el limite de mis letras al número al que pertenecen mayor o igual
+            c = String.fromCharCode(((codigo - 65 + offset) % 26) + 65); //ejecuto mi formula con variable de nombre c junto con el offset por ahora, para letras mayuscúlas
+        } else if ((codigo >= 97) && (codigo <= 122)) { //ejecuto mi formula junto con el offset por ahora, para letras minúsculas
+            c = String.fromCharCode(((codigo - 97 + offset) % 26) + 97);
+        } else if (codigo == 32) {
+            // c = String.fromCharCode(((codigo - 32 + offset) % 26) + 32);
+            c = " "; //aquí le digo a mi código  que cuando haya un espacio lo respete y lo deje tal cuál
         }
-        // Se muestra el resultado en pantalla.
-    }
-    document.write('El resultado es: ' + output);
+        console.log(c);
+        resultado += c;
+    });
+    console.log(resultado); //probando en consola
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ A  B  C  D  E  F  G  H  I  J   K  L  M
 65 66 67 68 69 70 71 72 73 74 75 76 77   
 
 N  O  P  Q  R  S  T  U  V  W  X  Y  Z
-78 79 80 81 82 83 84 85 86 87 88 89 90
+78 79 80 81 82 83 84 85 86 87 88 89 
+
+
+
+a              z
+97......     122
 
 (CODIGO DE LA LETRA +  DESLOCAMENTO) % TAM DO ALFABETO) 
 POSICIÓN DE LATRA + VALOR FIJO)% RESIDUO DE 26
@@ -40,40 +45,14 @@ FromCharCode: me muestra las letras
 
 
 //let String = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz';
-//document.write(String.charCodeAt(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
-
-/*let cipher = document.getElementById('cifrar'),
-    descipher = document.getElementById('descifrar'),
-    input = document.getElementById('mensajeInput'),
-    show = document.getElementById('show'),
-    number = document.getElementById('key');
-
-cifrarbotooonc.addEventListener('click', cifrar);
-
-function cipher() {
-    let newTxt = '';
-    let inpuTxt = input.value;
-    let key = parseInt(number.value);
-    for (i = 0; i < inpuTxt.length; i++) {
-        let toAscii = inpuTxt.charCodeAt(i);
-        if (toAscii >= 65 && toAscii <= 90) {
-            newTxt += String.fromCharCode((toAscii - 65 + key) % 26 + 65); // Mayus
-            descipher.className = 'visible';
-        } else if (toAscii >= 97 && toAscii <= 122) {
-            newTxt += String.fromCharCode((toAscii - 97 + key) % 26 + 97); // Minus
-            descipher.className = 'visible';
-        } else
-            show.innerHTML = `<p> Resultado: ${newTxt} </p>`;
-    }
-    show.innerHTML = `<p> Resultado: ${newTxt} </p>`;
-}*/
+//document.write(String.charCodeAt(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))*/
 
 let buttoncifrar = document.getElementById("cifrar");
 let buttondescifrar = document.getElementById("descifrar");
 let buttoncomprar = document.getElementById("comprar");
 
 buttoncifrar.onclick = function() {
-    alert("gggg");
+    cifrar();
 }
 
 buttondescifrar.onclick = function() {
