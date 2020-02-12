b7815f37f76bd73af66d0b8da452757feb696896
Mara Mulato <marianamendozamulato@MacBook-Mara.local>
2020-02-12 09:38:15 -0600

    hasta ahora, pantallas y diseño

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+//este es el cambio de pantalla
  function pantalla2() {
    let pantallas = document.getElementById('pantalla1')
    pantallas.style = "display:none";
    document.getElementById('pantalla2').style.display = "block";}
 
-document.getElementById('ir').addEventListener('click',pantalla2)
+document.getElementById('ir').addEventListener('click',pantalla2);
+
+
+
+
+
+
+
+//esto es lo que ya estaba
+//import cipher from './cipher.js'
+//console.log(cipher);
