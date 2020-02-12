38b69a00b9175d241c5be88d200b12e0a977f757
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-02-12 12:56:09 -0600

    se termino la función para hacer la entrada del nombre de usuario solo en mayúsculas y tambien se anexo a la palabra clave. Se le dio funcionalidad al botón Home

--- src/index.js
+++ src/index.js
@@ -1,35 +1,60 @@
+//import cipher from './cipher.js';
+
+console.log(cipher);
+
 function startApp(){
-  document.getElementById("home").style.display = "none";
-  document.getElementById("number").style.display = "block";
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('number').style.display = 'block';
   }
 
 function getOffsetNo(){
   let offsetNo = document.getElementById('dayNo').value;
   if (offsetNo <32 && offsetNo >0) {
-    document.getElementById('number').style.display = "none";
-    document.getElementById('user').style.display = "block";
+    document.getElementById('number').style.display = 'none';
+    document.getElementById('user').style.display = 'block';
   }
   else {
-    alert('Ingresa un número del 1 al 31');
+    alert('Solo del 1 al 31');
   }
   }
 
 function getUser(){
   let userName = document.getElementById('name').value;
-  document.getElementById('test').innerHTML = userName + ' chan';
+  if (userName != ''){
+  document.getElementById('user').style.display = 'none';
+  document.getElementById('keyword').style.display = 'block';
+  }
+  else {
+  alert('Escribe tu usuario')
+  }
 }
 
-function NoSpace(event) {
-    let space = event ? event.which : window.event.keyCode;
-    if (space < 64  && space > 91) return false;
+function capsOnly(event) {
+    let pressed = event.which;
+    if (pressed < 64) return false;
+    if (pressed > 91) return false;
   }
 
+  function getKey(){
+    let key = document.getElementById('pass').value;
+    if (key != '') {
+    document.getElementById('keyword').style.display = 'none';
+    document.getElementById('results').style.display = 'block';
+    }
+    else {
+    alert('Escribe la palabra clave')
+    }
+  }
+
+function reStart(){
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('home').style.display = 'block';
+  
+}
+
+
 let cipheredUser = 'ciphered user';
-document.getElementById("ciphered").value = cipheredUser;
+document.getElementById('ciphered').value = cipheredUser;
 
 let decipheredKeyword = 'deciphered keyword';
-document.getElementById("deciphered").value = decipheredKeyword;
-
-//import cipher from './cipher.js';
-
-//console.log(cipher);
+document.getElementById('deciphered').value = decipheredKeyword;
