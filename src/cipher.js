9aa6ba34f34cf620fd6a4d7895df1cf587ac4940
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-02-11 22:30:03 -0600

    practicando con las variables y funciones en js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
-const cipher = {
+let textToCipher = document.getElementById('textoACifrar');
+let numOfPositions = document.getElementById('numDePosiciones');
+let text = '';
+let number = '';
+let textToAscii='';
+
+function cifrado () {
+  text = (document.getElementById('textoACifrar').value);
+  number = (document.getElementById('numDePosiciones').value);
+  
+    for(i=0; i <= text.length; i++) {
+      textToAscii = text.charCodeAt(i);
+      alert(textToAscii);
+    };
+
+};
+
+
+
+/*const cipher = {
   // ...
 };
 
 export default cipher;
+*/
