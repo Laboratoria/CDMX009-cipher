78349a8f653d89027842c332297500b52bcc024a
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-02-11 18:10:31 -0600

    Se cambiaron los div por section, se añadieron funciones para cambiar de la primera pantalla a la segunda. Se añadio función para restringir el ingreso de números a solo de 1 a 31. Se añadio funcion para ir a la siguiente pantalla. Se esta añadiendo función para restringir uso de minusculas, espacios y otros simbolos que no sean mayúsculas.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-function testTwo(){
-  document.getElementById('test').innerHTML = '¡Hola Mundo!';
+function startApp(){
+  document.getElementById("home").style.display = "none";
+  document.getElementById("number").style.display = "block";
+  }
+
+function getOffsetNo(){
+  let offsetNo = document.getElementById('dayNo').value;
+  if (offsetNo <32 && offsetNo >0) {
+    document.getElementById('number').style.display = "none";
+    document.getElementById('user').style.display = "block";
+  }
+  else {
+    alert('Ingresa un número del 1 al 31');
+  }
+  }
+
+function getUser(){
+  let userName = document.getElementById('name').value;
+  document.getElementById('test').innerHTML = userName + ' chan';
 }
 
-let cipheredUser = 'AGUANTÁ';
-document.getElementById("ciphered").value = cipheredUser;
-
-let decipheredKeyword = 'TODAVIA NO SIRVE';
-document.getElementById("deciphered").value = decipheredKeyword;
+function NoSpace(event) {
+    let space = event ? event.which : window.event.keyCode;
+    if (space >= 65) return false;
+  }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+
+
+let cipheredUser = 'ciphered user';
+document.getElementById("ciphered").value = cipheredUser;
+
+let decipheredKeyword = 'deciphered keyword';
+document.getElementById("deciphered").value = decipheredKeyword;
+
 import cipher from './cipher.js';
 
 console.log(cipher);
