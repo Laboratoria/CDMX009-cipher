4681702736409175040aa004f48887695a206d3c
EdithOrt <eoc.diseno@gmail.com>
2020-02-12 09:30:23 -0600

    últimos cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,7 @@
-/*let mensaje= document.getElementById ("cajaMensaje").value;
-let desplazamiento= document.getElementById("cajaClave").value;
-
-
-if cifrar() {
-(mensaje.length=" "){
-    document.getElementById("advertencia2").innerText= "Ingresar mensaje."
- }
-    else if (mensaje==50.length){ 
-        (mensaje.charcodeAt -65 + desplazamiento)%26+65;
-        document.getElementById("pantallaUno").style.display="none";
-        document.getElementById("pantallaDos").style.display="block";
-
- }
- console.log(cifrar)
-}
-*/
-
-
 string= document.getElementById ("cajaMensaje").value;
 
 
 function cifrar(){
-    document.getElementById("pantallaDos").style.display="none";
-    document.getElementById("pantallaTres").style.display="block";
     //mensaje= string.charCodeAt();
     //return mensaje;
 
