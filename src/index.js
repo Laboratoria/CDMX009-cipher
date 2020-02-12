31542d22e405e7919c99aee3297c31a02d31412b
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-02-11 22:12:26 -0600

    index.js first functions update

--- src/index.js
+++ src/index.js
@@ -6,21 +6,41 @@ let bluetoothSwitchStatus = document.getElementById("bluetoothSwitch");
 bluetoothSwitch(bluetoothSwitchStatus);
 bluetoothSwitchStatus.addEventListener("click", bluetoothSwitch);
 
+let contactsSelectStatus = document.getElementById("contacts");
+contactsSelect(contactsSelectStatus);
+contactsSelectStatus.addEventListener("change", contactsSelect);
+
 function bluetoothSwitch() {
     if (bluetoothSwitchStatus.checked == true){
         console.log("ON");
         document.getElementById("contacts").disabled = false;
-        
     }else{
         console.log("OFF");
-        let BtStatusElement = document.getElementsByClassName("BtStatus");
-        console.log(BtStatusElement.length);
-        //BtStatusElement[0].disabled = true;
-        for(let i=0; i< 9; i++){
-            console.log(BtStatusElement[i]);
-            BtStatusElement[i].disabled = true;
-            console.log("Si entra al for");
-        }
+        let statusElement = document.getElementsByClassName("btStatus");
+        disabledElements(statusElement);
     }
     console.log(bluetoothSwitchStatus.checked);
+}
+
+function contactsSelect() {
+    if(contactsSelectStatus.value != "contactsTitle"){
+        console.log("select entra");
+        let statusElement = document.getElementsByClassName("contactsStatus");
+        ableElements(statusElement);
+    }else{
+        let statusElement = document.getElementsByClassName("contactsStatus");
+        disabledElements(statusElement);
+    }
+}
+
+function disabledElements(statusElement) {
+    for(let i=0; i<statusElement.length; i++){
+        statusElement[i].disabled = true;
+    }
+}
+
+function ableElements(statusElement) {
+    for(let i=0; i<statusElement.length; i++){
+        statusElement[i].disabled = false;
+    }
 }
