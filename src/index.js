d47566a6c6a65dcff7747ab9087315a6408c78c7
LizethRivera04 <60666768+LizethRivera04@users.noreply.github.com>
2020-02-13 10:58:27 -0600

    Se agregaron espacios a las cadenas para cifrar y descifrar

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ function cifrar(){
   string = string.toUpperCase();
   for (var i = 0; i < string.length; i++) {
     let letterCode = string[i].charCodeAt(0);
+    let space = letterCode == 32
+    if (letterCode >= 65 && letterCode <= 90) {
     //console.log(letterCode)
     //document.getElementById('resultadocif').innerHTML += letterCode;//Se obtuvo el código Ascii de la cadena ingeresada y se imprimieron
     let newCode = (letterCode - 65 + offset) %26 + 65
@@ -18,8 +20,18 @@ function cifrar(){
     console.log(offset)
     let newLetter = String.fromCharCode(newCode)
     document.getElementById('resultadocif').innerHTML += newLetter;
+      
+    }
+    if (space) {
+      newCode = (32) %65
+      let newLetter = String.fromCharCode(newCode)
+      document.getElementById('resultadocif').innerHTML += newLetter;
+      
+    }
+
   }
 }
+
 
  function descifrar(){
   
@@ -27,13 +39,25 @@ function cifrar(){
     string = string.toUpperCase();
     for (var i = 0; i < string.length; i++) {
       let letterCode = string[i].charCodeAt(0);
-      //console.log(letterCode)
-      //document.getElementById('resultadocif').innerHTML += letterCode;//Se obtuvo el código Ascii de la cadena ingeresada y se imprimieron
-      let newCode = (letterCode + 65 - offset) %26 + 65
+      let space = letterCode == 32
+      if (letterCode >= 65 && letterCode <= 90) {
+        let newCode = (letterCode + 65 - offset) %26 + 65
       //console.log(newCode);
       console.log(offset);
       let newLetter = String.fromCharCode(newCode);
       document.getElementById('resultadodes').innerHTML += newLetter;
+      //console.log(letterCode)
+      //document.getElementById('resultadocif').innerHTML += letterCode;//Se obtuvo el código Ascii de la cadena ingeresada y se imprimieron
+      }
+      if (space) {
+      newCode = (32) %65
+      let newLetter = String.fromCharCode(newCode);
+      document.getElementById('resultadodes').innerHTML += newLetter;
+        
+      }
+      
+
+      
     }
 }
 //console.log(cipher);
