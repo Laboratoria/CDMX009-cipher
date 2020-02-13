d0f68ff26871581e9c57e82028a7c4151eb206eb
EstherManrique <esmango0610@gmail.com>
2020-02-13 09:04:39 -0600

    Mi segundo commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,82 @@
 const cipher = {
-  // ...
+
+paterno : document.getElementById('cifrar').value,
+mes : parseInt(document.getElementById('movimientos').value), 
+
+cifrado : function(paterno, mes) {
+    mes = parseInt(mes % 26);    
+
+    let datoMayusculas = paterno.toUpperCase();
+    let alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let datoCifrado = '';
+
+    for(let i = 0; i < datoMayusculas.length; i++) {
+        let letraActual = datoMayusculas[i];
+        if(letraActual === ' ') {
+            datoCifrado += letraActual;
+            continue;
+        }
+
+        let actualIndex = alfabeto.indexOf(letraActual);
+        let nuevoIndex = actualIndex + mes;
+
+        if(nuevoIndex > 25) {
+            nuevoIndex = nuevoIndex - 26;
+        } else if(nuevoIndex < 0) {
+            nuevoIndex = nuevoIndex + 26;
+        } else if(paterno[i] === paterno[i].toUpperCase()) {
+            datoCifrado += alfabeto[nuevoIndex].toUpperCase();
+        } else {
+            datoCifrado += alfabeto[nuevoIndex]
+        }
+    }
+    return datoCifrado;
+},
+
+
+desCifrado : function(paterno, mes) {
+    mes = parseInt(mes % 26);    
+
+    let datoMayusculas = paterno.toUpperCase();
+    let alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let datoDesCifrado = '';
+
+    for(let i = 0; i < datoMayusculas.length; i++) {
+        let letraActual = datoMayusculas[i];
+        if(letraActual === ' ') {
+            datoDesCifrado += letraActual;
+            continue;
+        }
+
+        let actualIndex = alfabeto.indexOf(letraActual);
+        let nuevoIndex = actualIndex - mes;
+
+        if(nuevoIndex > 25) {
+            nuevoIndex = nuevoIndex - 26;
+        } else if(nuevoIndex < 0) {
+            nuevoIndex = nuevoIndex + 26;
+        } else if(paterno[i] === paterno[i].toUpperCase()) {
+            datoDesCifrado += alfabeto[nuevoIndex].toUpperCase();
+        } else {
+            datoDesCifrado += alfabeto[nuevoIndex]
+        }
+    }
+    return datoDesCifrado;
+}
 };
+
+
+
+
+//console.log(cipher(paterno, mes));
+
+
+
+
+//'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//'abcdefghijklmnopqrstuvwxyz'
+
+  
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
+let paterno = '';
+let mes = 0;
+let cifrar = document.getElementById('botonCifrar');
+let desCifrar = document.getElementById('botonDesCifrar');
+
+function obtenerDatos() {
+    paterno = document.getElementById('cifrar').value;
+    mes = parseInt(document.getElementById('movimientos').value);
+}
+
+cifrar.onclick = function() {
+    obtenerDatos(paterno, mes);
+    document.getElementById('resultadoCifrado').value = cipher.cifrado(paterno, mes);
+
+    document.getElementById('resultadoCifrado').innerHTML = cipher.cifrado(paterno, mes);
+};
+
+desCifrar.onclick = function() {
+    obtenerDatos(paterno, mes);
+    document.getElementById('resultadoDesCifrado').value = cipher.desCifrado(paterno, mes);
+
+    document.getElementById('resultadoDesCifrado').innerHTML = cipher.desCifrado(paterno, mes);
+} 
+
 console.log(cipher);
+
