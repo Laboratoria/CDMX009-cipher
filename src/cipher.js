6ace85e47b309f547ad6a3cb312014889ffeca61
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-02-12 23:48:35 -0600

    added encrypt function work with hardcoded parameters

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,22 @@
 let cipher = {
-  encrypt: function(password, scrollNumber) {
-    let result = password + scrollNumber;
-    return result;
+  encryptPassword: function(stringPassword, scrollNumber) {
+    let encryptResult = "";
+    for (let i = 0; i < stringPassword.lenght; i++) {
+      console.log("i", stringPassword[i]);
+      let codeCharacter = stringPassword.charCodeAt(i);
+      console.log("codeCharacter", codeCharacter)
+      let firstUppercaseNumber = 65;
+      let lastUppercaseNumber = 90;
+      let totalCharacterAlphabetNumber = 26;
+      if (codeCharacter >= lastUppercaseNumber && codeCharacter <= lastUppercaseNumber) {
+       encryptResult += String.fromCharCode(codeCharacter - firstUppercaseNumber + scrollNumber) % totalCharacterAlphabetNumber + firstUppercaseNumber;
+       console.log("encrypt result", encryptResult)
+      }
+    }
   },
-  desencrypt: function(password, scrollNumber) {
-    let result = password + scrollNumber;
-    return result;
+  desencryptPassword: function(stringPassword, scrollNumber) {
+    console.log(stringPassword)
+    console.log(scrollNumber)
  }
 };
-
 export default cipher;
