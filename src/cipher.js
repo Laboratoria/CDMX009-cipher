ee4232951d8105fcd4c9fd2998b970a97887f803
Gaby Alvarez <gabyalvarzb@gamil.com>
2020-02-12 23:18:40 -0600

    Mejoras HTML y css

--- src/cipher.js
+++ src/cipher.js
@@ -4,19 +4,31 @@ const cipher = {
         for (var index = 0; index < textOrigen.length; index++) {
             var caracter = textOrigen.charAt(index);
             var caracterASCII = caracter.charCodeAt(0);
-            var newCaracterASCII = ((caracterASCII - 65 + offset) % 26 + 65);
-            newCadena += String.fromCharCode(newCaracterASCII);
+
+            if (caracter == " " || caracterASCII < 65 || caracterASCII > 90) {
+                newCadena += textOrigen.charAt(index);
+            } else {
+                var newCaracterASCII = ((caracterASCII - 65 + offset) % 26 + 65);
+                newCadena += String.fromCharCode(newCaracterASCII);
+            }
         }
 
         return newCadena;
     },
     decode(offset, textOrigen) {
         var newCadena = "";
+
         for (var index = 0; index < textOrigen.length; index++) {
             var caracter = textOrigen.charAt(index);
             var caracterASCII = caracter.charCodeAt(0);
-            var newCaracterASCII = ((caracterASCII - 65 - offset) % 26 + 65);
-            newCadena += String.fromCharCode(newCaracterASCII);
+
+            if (caracterASCII == 32 || caracterASCII < 65 || caracterASCII > 90) {
+                newCadena += textOrigen.charAt(index);
+            } else {
+                var newCaracterASCII = ((caracterASCII - 65 - offset) % 26 + 65);
+                newCadena += String.fromCharCode(newCaracterASCII);
+            }
+
         }
 
         return newCadena;
