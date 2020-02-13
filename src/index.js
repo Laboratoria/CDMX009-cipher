ad2699b4fbf5839ccf2f0d59a5d2cf71d4425d21
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-02-12 22:21:23 -0600

    avance día 12

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+
 function pantalla2(){
+
 let screen= document.getElementById("screen1");
 screen.style="display:none";
-document.getElementById("pantalla2").style.display= "block";
+document.getElementById("screen2").style.display= "block";
+}
+function pantallaInicio(){
+
+  let screen= document.getElementById("screen2");
+  screen.style="display:none";
+  document.getElementById('screen1').style.display="block";
+}
+function pantalla3(){
+
+let screen= document.getElementById("screen1");
+screen.style="display:none";
+document.getElementById("screen3").style.display= "block";
+}
+function pantallaInicio1(){
+let screen= document.getElementById("screen3");
+  screen.style="display:none";
+document.getElementById('screen1').style.display="block";
 }
