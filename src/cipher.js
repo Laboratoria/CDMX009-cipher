e1d493986efb202ec8f2fbd52c5e5374af95ab73
akaren29 <akjassoj@outlook.com>
2020-02-13 09:58:08 -0600

    Se agrego la funcion y el manejo del dom en Index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,35 @@
-const alpabeth = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
-const posicion = 2
 
-letra = prompt
+const cipher = {
+  encode: (offset, string) => {//offset es el número de desplazamiento
+    //string es el mensaje a cifrar
+  let codeCipher, position; //codecipher ->codigo ascci de letra 
+  //position ->posicion de la letra cifrada en el alfabeto
+  let messResult = '';//Resultado de mensaje descifrado
 
-
-/* const cipher = {
-  // ...
+   for (let i=0; i <string.length; i++){ //Recorrido de los caracteres del string
+     if(string[i] === ' '){ //Si el string tiene un espacio se deja igual
+        messResult += ' ';
+     }
+     else{
+       const codeAscii = string.charCodeAt(i);//Obtiene codigo Ascii de la letra
+         if(codeAscii > 90){
+           //Formula para Minusculas de 97-122
+           position = codeAscii - 97 + (offset % 26);
+           //Formula para obtener el codigo ascci en 97 empieza la a minuscula
+           codeCipher = (position % 26) + 97;
+           //Para Mayusculas de 65-90
+         }else{
+           position = codeAscii -65 + (offset % 26);
+           codeCipher = (position % 26) + 65;
+         }
+          const letterCipher = string.fromCharCode(codeCipher);
+          //Obtiene la letra que corresponde el codigo Ascii, ya cifrado
+          messResult += letterCipher;
+         }
+     }
+     return messResult;
+   }
+  
 };
 
-export default cipher;/ */
+export default cipher; 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import cipher from './cipher.js';
+ import cipher from './cipher.js';
+ 
+ const getData = (elegirFuncion) => 
+ {
+  const offset = parseInt(document.getElementById('posicion').value);//Desplazamiento
+  //condicion if para ejecutar si es verdadero o falso
+  if(elegirFuncion){
+     const string = document.getElementById('mensaje').value;//Obtener el texto para cifrar
+     document.getElementById('messResult').value=cipher.encode(offset, string);
+     //Imprime el resultado del texto
+ }
+}
 
-console.log(cipher);
+ console.log(cipher);
