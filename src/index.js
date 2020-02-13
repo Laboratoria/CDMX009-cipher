6898d01f4a80fd51e3a3f66d12f3bf5ae154fc7f
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-02-12 23:53:18 -0600

    call encrypt password function and pass parameters

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 // alias for import
 import Cipher from './cipher.js';
+// let inputPassword = ""
+// if (inputPassword.length == 0 || inputPassword.length == " ") {
+//   alert("No ingresaste tu contraseña");
+// }
 
-function showEncryptPage() {
-    console.log( Cipher.encrypt('my password', 13) );
+function showEncryptPage(str, num) {
+    console.log( Cipher.encryptPassword(str,num) );
     document.getElementById("logins-second-page").style.display = "block";
-    document.getElementById("logins-first-page").style.display = "none";
+    document.getElementById("logins-main-page").style.display = "none";
     document.getElementById("logins-third-page").style.display = "none";
 }
 
-function showDesencryptPage() {
-    console.log( Cipher.desencrypt('my password encryptado', 13) );
-    document.getElementById("logins-first-page").style.display = "none";
+function showDesencryptPage(str, num) {
+    console.log( Cipher.desencryptPassword(str, num) );
+    document.getElementById("logins-main-page").style.display = "none";
     document.getElementById("logins-second-page").style.display = "none";
     document.getElementById("logins-third-page").style.display = "block";
 }
