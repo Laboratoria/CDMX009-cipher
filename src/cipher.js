60efcb7d76ac747a58e002807cde00f0fb45b8c5
Sheiki1 <kirokomerteabesos@gmail.com>
2020-02-13 13:38:20 -0600

    ya funcionan los if else

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,72 @@
-const cipher = {
-  encode,
-  decode
-};
 
+export const cipher= {
+    encode,
+    decode
+    //clean
+
+}
+function encode(offset, text){
+
+  let characters = [];
+  let concatenarCharacters="";
+  let charnum=0;
+
+  for (let i = 0; i < text.length; i++){
+      charnum = Number.parseInt(text.charAt(i).charCodeAt(0));
+      console.log(charnum);
+
+      if ((charnum==32)) {
+          charnum=charnum;
+          console.log("x");
+
+      }else if(65<=charnum && charnum<=90){
+              charnum = (charnum -65 + offset)%26+65;
+              console.log("mayusculas");
+
+      }else if((97<=charnum && charnum<=122)){
+              charnum=(charnum-97+ offset)%26+97;
+              console.log("minusculas");
+      }
+      characters[i]=String.fromCharCode(charnum);
+          concatenarCharacters += characters[i]+"";
+  }
+  return concatenarCharacters;
+ 
+}
+
+ function decode(offset, text){
+    
+  let characters = [];
+  let charnum=0;
+  let concatenarCharacters="";
+  
+  for (let i = 0; i < text.length; i++){
+      charnum= Number.parseInt(text.charAt(i).charCodeAt(0));
+      console.log(charnum);
+      if ((charnum==32)) {
+          charnum=charnum;
+          console.log("x");
+          
+      } else if((65<=charnum && charnum <=90)){
+          charnum = (charnum +65 - offset)%26+65;
+          console.log("mayusculas");
+          
+      }else if(97<=charnum && charnum <=122) {
+          charnum=(charnum+97- offset)%26+97;
+          console.log("minusculas");
+         }
+  characters[i]=String.fromCharCode(charnum);
+  concatenarCharacters += characters[i]+"";
+  }
+  return concatenarCharacters;
+  
+}
+/*
+function clean (){
+    document.getElementById("resultado").innerText="";
+  }*/
 export default cipher;
+
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,74 +1,56 @@
 
+import   cipher from "./cipher.js" ;
 
-function cifrar(){
-    let cipherText = document.getElementById("cipherText");
-    text = cipherText.value;
-             
-    let cipherKey = document.getElementById("cipherKey");
-    offset = parseInt(cipherKey.value);
+let encodeButton = document.getElementById("encodeButton");
+encodeButton.onclick = encode1;
+
+let decodeButton = document.getElementById("decodeButton");
+decodeButton.onclick = decode1;
+
+let cleanButton = document.getElementById("clean");
+cleanButton.onclick = clean;
+
+
+function encode1(){
+let cipherText = document.getElementById("cipherText");
+let text = cipherText.value;
     
-    let caracteres = [];
-    let concatenarCaracteres="";
-    let charnum=0;
-  
-    for (let i = 0; i < text.length; i++){
-        charnum = Number.parseInt(text.charAt(i).charCodeAt(0));
-        console.log(charnum);
+let cipherKey = document.getElementById("cipherKey");
+let offset = parseInt(cipherKey.value);
 
-        if ((charnum==32)) {
-            charnum=charnum;
-            console.log("x");
+let concatenarCharacters = cipher.encode(offset,text);
 
-        }else if(64<charnum<91){
-                charnum = (charnum -65 + offset)%26+65;
-                console.log("mayusculas");
-
-        }else if((96<charnum<123)){
-                charnum=(charnum-97+ offset)%26+97;
-                console.log("minusculas");
-        }
-            caracteres[i]=String.fromCharCode(charnum);
-            concatenarCaracteres += caracteres[i]+"";
-    }          
-    let element = document.getElementById("resultado");
-    element.innerHTML=concatenarCaracteres.toString();
+let element = document.getElementById("resultado");
+element.innerHTML = concatenarCharacters.toString();
 }
 
-function descifrar() {
+function decode1(){
     let cipherText = document.getElementById("cipherText");
-    text = cipherText.value;
-                
+    let text = cipherText.value;
+        
     let cipherKey = document.getElementById("cipherKey");
-    offset = parseInt(cipherKey.value);
-
-    let caracteres = [];
-    let charnum=0;
-    let concatenarCaracteres="";
+    let offset = parseInt(cipherKey.value);
     
-    for (let i = 0; i < text.length; i++){
-        charnum= Number.parseInt(text.charAt(i).charCodeAt(0));
-        console.log(charnum);
-        if ((charnum==32)) {
-            charnum=charnum;
-            console.log("x");
-            
-        } else if((64<charnum<91)){
-            charnum = (charnum +65 - offset)%26+65;
-            console.log("mayusculas");
-            
-        }else if(96<charnum<123) {
-            charnum=(charnum+97- offset)%26+97;
-            console.log("minusculas");
-           }
-    caracteres[i]=String.fromCharCode(charnum);
-    concatenarCaracteres += caracteres[i]+"";
+    let concatenarCharacters = cipher.decode(offset,text);
+    
+    let element = document.getElementById("resultado");
+    element.innerHTML = concatenarCharacters.toString();
     }
-                  
-    let element = document.getElementById("resultado");
-    element.innerHTML = concatenarCaracteres.toString();
-    
-}
 
-function limpiar() {
+    function clean (){
+        document.getElementById("resultado").innerText="";
+    }
+
+console.log(cipher);
+
+/*
+let clean=document.getElementsByClassName("clean");
+clean(clean);
+clean.addEventListener("click",clean);
+
+function clean (){
     document.getElementById("resultado").innerText="";
+
   }
+export default cipher;*/
+
