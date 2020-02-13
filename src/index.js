c51b1db9488d1fdcf00f07d97c6477e10b09be3e
LizethRivera04 <60666768+LizethRivera04@users.noreply.github.com>
2020-02-12 18:45:48 -0600

    Se agregó JS

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
 //import cipher from './cipher.js';
-
+let offset = 0
 function saveNumber(){
   offset = parseInt(document.getElementById('posiciones').value); //localStorage
   alert("Hola " + offset);
-
-
 }
 
+
 function cifrar(){
-  var string = document.getElementById('cadenacif').value;
-
-  //let newString = code(string,offset)
-
+  let string = document.getElementById('cadenacif').value;
   string = string.toUpperCase();
   for (var i = 0; i < string.length; i++) {
-    var letterCode = string[i].charCodeAt(0);
+    let letterCode = string[i].charCodeAt(0);
     //console.log(letterCode)
-    document.getElementById('resultadocif').innerHTML += letterCode;
-    
+    //document.getElementById('resultadocif').innerHTML += letterCode;//Se obtuvo el código Ascii de la cadena ingeresada y se imprimieron
+    let newCode = (letterCode - 65 + offset) %26 + 65
+    //console.log(newCode)
+    console.log(offset)
+    let newLetter = String.fromCharCode(newCode)
+    document.getElementById('resultadocif').innerHTML += newLetter;
   }
-  //var posAscii = string.charCodeAt(0); //Cambiar posAscii por letterCode
-  
-  
-  
+}
 
+ function descifrar(){
+  
+    let string = document.getElementById('cadenacif').value;
+    string = string.toUpperCase();
+    for (var i = 0; i < string.length; i++) {
+      let letterCode = string[i].charCodeAt(0);
+      //console.log(letterCode)
+      //document.getElementById('resultadocif').innerHTML += letterCode;//Se obtuvo el código Ascii de la cadena ingeresada y se imprimieron
+      let newCode = (letterCode + 65 - offset) %26 + 65
+      //console.log(newCode);
+      console.log(offset);
+      let newLetter = String.fromCharCode(newCode);
+      document.getElementById('resultadodes').innerHTML += newLetter;
+    }
 }
 //console.log(cipher);
 
-
-
-  
