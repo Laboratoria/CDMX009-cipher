f548062dc6289a7f309cb636a2ee054624000a98
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-02-13 13:11:01 -0600

    se realizaron las funciones. se modifico una clase en css

--- src/cipher.js
+++ src/cipher.js
@@ -1,37 +1,68 @@
-//variable que guarda el elemento textarea
-let textToCipher = document.getElementById('textoACifrar');
-//variable que guarda el elemento number de las posiciones
-let numOfPositions = document.getElementById('numDePosiciones');
-//variable que va a guardar el texto ingresado en textoACifrar
-let text = '';
-//variable que va a guardar el numero de posiciones ingresadas
-let number = '';
-//guarda el texto convertido a ascii
-let textToAscii='';
 
-//funcion para realizar el cifrado
-function cifrado () {
-  text = (document.getElementById('textoACifrar').value).toUpperCase();
-  number = (document.getElementById('numDePosiciones').value);
+function encode () {
+  //variables donde se guardan los datos del formulario
+let text = (document.getElementById('textoACifrar').value).toUpperCase();
+let offset = parseInt(document.getElementById('numDePosiciones').value);
+//ciclo para asignar el numero de letras
+  for (let i=0; i < text.length; i++) {
+    let textToAscii = text.charCodeAt (i);
+//ciclo que va a limitar ascii a mayusculas y dar texto cifrado
+    if (textToAscii >= 65 && textToAscii <= 90) {
+      let formula = (textToAscii-65+offset)%26+65;
+      let cipherText = String.fromCharCode(formula);
+      document.write (cipherText);
+    }
+ //ciclo que va a dar los espacios donde corresponden   
+    else if (textToAscii === 32) {
+      let space = String.fromCharCode(textToAscii);
+      document.write(space);
+    }
+  }
+}
+
+function decode () {
+  //variables donde se guardan los datos del formulario
+  let text = (document.getElementById('textoADescifrar').value).toUpperCase();
+  let offset = parseInt(document.getElementById('numDePosicionesDes').value);
+  //ciclo para asignar el numero de letras
+    for (let i=0; i < text.length; i++) {
+      let textToAscii = text.charCodeAt (i);
+  //ciclo que va a limitar ascii a mayusculas y dar texto descifrado
+      if (textToAscii >= 65 && textToAscii <= 90) {
+        let formula = (textToAscii+65-offset)%26+65;
+        let cipherText = String.fromCharCode(formula);
+        document.write (cipherText);
+      }
+   //ciclo que va a dar los espacios donde corresponden   
+      else if (textToAscii === 32) {
+        let space = String.fromCharCode(textToAscii);
+        document.write(space);
+      }
+    }
+}
+
+/*funcion para realizar el cifrado
+function cipher () {
+  //asigna los datos y convierte a mayusculas
+  let text = (document.getElementById('textoACifrar').value).toUpperCase();
+  let number = parseInt(document.getElementById('numDePosiciones').value);
   
-  //para convertir a ascii y a mayusculas
+  //para convertir a ascii 
     for(i=0; i < text.length; i++) {
       //texto convertido en ascii
-      textToAscii = text.charCodeAt(i);
-      //formula para conseguir ascii
-      let temporal = (textToAscii-65+number)%26+65;
-      //alert(temporal);
-      //convertir a letra
-      let cipher = String.fromCharCode(temporal);
-      document.write (cipher);
+      let textToAscii = text.charCodeAt(i);
+
+
       
-      //let positions = (textToAscii+number)%26;
-      //let cipherMessage = String.fromCharCode(positions)
-      //alert(positions);
+        if (textToAscii === 32) {
+          let space = String.fromCharCode(getAscii);
+        }
       
-      //alert(textToAscii);
-      //let cipher = String.fromCharCode(i);
-      //alert(cipher);
+      //formula para conseguir posiciones en ascii
+      let getAscii = (textToAscii-65+number)%26+65;
+            //convertir a letra
+      let cipherString = String.fromCharCode(getAscii);
+      document.write (cipherString);
     };
 
 };
