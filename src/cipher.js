f11572cdce083a3ad30fc8672f310ae327d8259b
abigail-sanlo <abisl9160@gmail.com>
2020-02-14 13:18:37 -0600

    avance proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
 const cipher = {
-  // ...
-};
+   encode:(despderecha, msjusuario) => {
+    let textcif = "";
+    for (let i = 0; i < msjusuario.length; i++) {
+      let character=message[i];
+    if (character.match(/[a-z]/i)){
+      let textMayu=msjusuario.toUpperCase();
+      let msj2=(textMayu.charCodeAt(i)- 65 + parseInt(despderecha))%26+65;
+      textcif += String.FromCharCode=(msj2);
+    }
+
+    else{
+      textcif+=character;
+    }
+  }
+    return textcif;
+  },
+  decode: (despizquierda, msjusuario) => {
+    let textdecifrado= "";
+
+    for (let i=0;i < msjusuario.length;i++){
+      //characterD=message[i];
+      let textdecifrado=(msjusuario.charCodeAt(i)- 65 + parseInt(despizquierda))%26+65;
+      let result= String.FromCharCode=(textdecifrado);
+      if ()
+      textdecifrado=result;
+    }else {
+      textdecifrado+=characterD;
+    }
+    return textdecifrado;
+  }
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js"
+let cifBoton = document.getElementById('cifrar');
+cifBoton.addEventListener("click",() => {
+  let msjusuario = document.getElementById("ingresoMensaje").value;
+  let despderecha = document.getElementById("nOffsetD").value;
+  let resultado = cipher.encode(despderecha, msjusuario);
+  document.getElementById("resultadoCif").innerHTML = resultado;
+})
 
-console.log(cipher);
+let desBoton =document.getElementById("decifrado");
+desBoton.addEventListener("click",() => {
+  let msjusuario2=document.getElementById("ingresoMensaje").value;
+  let despizquierda=document.getElementById("n_offset_d").value;
+  let resultado2=cipher.decode(despizquierda,msjusuario2);
+  document.getElementById("resultadoCif").innerHTML=resultado2;
+})
