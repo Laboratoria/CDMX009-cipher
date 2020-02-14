7a64117d0ce0f3bebc991f811872d429b8b4fd72
andreatoledo <andytoledo.e03@gmail.com>
2020-02-14 12:57:36 -0600

    cambios en html

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,71 @@
-const cipher = {
-  // ...
+window.cipher = {
+  //crear una función de cifrado
+  encode: (offset, toEncode) => {
+    let result1 = '';
+    let toUnicode='';
+    let toUnicode1='';
+    let toUnicode2='';
+    //variable donde estara almacenado el ascii encriptado
+    //let toUnicode3='';
+    for(let i = 0; i< toEncode.length; i++){ //Convertiremos y alamacenamos cada letra de la frase ingresada a código Ascci
+    let toAscii = toEncode.charCodeAt(i); //Condicionamos para que la frase almacenada este en el rango de numeros ASCII y no de otros
+    //console.log(toAscii);
+    if (toAscii>=65 && toAscii<=90){
+    toUnicode = String.fromCharCode((toAscii- 65 + offset)%26 + 65);  //Si la letra es MAYÚSCULA Aplicaremos la formula de César para obtener el código ASCII de la letra ya encriptada
+    result1 += toUnicode; //codAsciiEncrypted: Guardamos el código ascii encriptado
+    }else if (toAscii === 32){
+    toAscii = ((toAscii- 32 + offset)%1 + 32); //valido si  hay un espacio  en ASCII 
+    toUnicode1  = String.fromCharCode(toAscii);
+    result1 += toUnicode1;
+    }else if (toAscii>=97 && toAscii<=122){
+    toUnicode2 = String.fromCharCode((toAscii- 97 + offset)%26 + 97); //Si la letra es minúscula Aplicaremos la formula de César para obtener el código ASCII de la letra ya encriptada
+    //codAsciiEncrypted: Guardamos el código ascii encriptado
+    result1 += toUnicode2;
+    //de la a-z
+    //}else if (toAscii === 165){
+    //toAscii = ((toAscii- 165 + offset)%1 + 165);
+    //toUnicode3 = String.fromCharCode(toAscii);
+    //result1 += toUnicode3;
+    //letra Ñ
+    // To ascii y to unicode: Estas funciones estan diseñadas para realizar la "traducción" de códigos de teclas virtuales + flags de estas + estado del teclado + configuración regional del teclado, a sus correspondientes teclas, estas proveen un mecanismo de detección de la tecla bastante efectivo y preciso, 
+    }
+  }
+  //console.log(result1);
+  return result1; //Por ultimo retornamos la nueva frase encriptada
+  },
+  decode: (offset, toDecode) => {
+    let result2 = '';
+    let toUnicode6='';
+    let toUnicode7='';
+    let toUnicode8='';
+    let toUnicode9='';
+    for(let i = 0; i< toDecode.length; i++){
+    let toAscii = toDecode.charCodeAt(i);
+    //console.log(toAscii);
+    if (toAscii>=65 && toAscii<=90){
+    toUnicode6 = String.fromCharCode((toAscii+ 65 - offset-26)%26 + 65);
+    result2 += toUnicode6;
+    //A-Z
+    }else if (toAscii === 32){
+    toAscii = ((toAscii+ 32 - offset-1)%1 + 32);
+    toUnicode7 = String.fromCharCode(toAscii);
+    result2 += toUnicode7;
+    //espacio
+    }else if (toAscii>=97 && toAscii<=122){
+    toUnicode8 = String.fromCharCode((toAscii+97 - offset-38)%26 + 97);
+    result2 += toUnicode8;
+    //a-z
+    }else if (toAscii === 165){
+    toAscii = ((toAscii- 165 + offset)%1 + 165);
+    toUnicode9 = String.fromCharCode(toAscii);
+    result2 += toUnicode9;
+    //letra Ñ
+    }
+  }
+  //console.log(result2);
+  return result2;
+  }
 };
 
-export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-
-import cipher from './cipher.js';
-
-console.log(cipher);
 //audio /document.getElementById("audio").value;
 //function myFunction() {
 //var x = document.getElementById("audio").autoplay;
 //document.getElementById("demo").innerHTML = x;
 //}
 
+
 let containerResult = document.getElementById('container');
-  
-document.getElementById("btnCifrar").addEventListener("click", () => {
+
+
+document.getElementById("btncifrar").addEventListener("click", () => {
 //console.log("hola");
   let toEncode= document.getElementById("message").value;
   let offset = parseInt(document.getElementById("offset").value);
   containerResult.innerHTML = cipher.encode(offset,toEncode);
 
-document.getElementById("btnDescifrar").addEventListener("click", () => {
-  let toDecode = document.getElementById("message").value;
-  let offset = parseInt(document.getElementById("offset").value);
-  containerResult.innerHTML = cipher.decode(offset,toDecode);
-//console.log("hola");
+
+  document.getElementById("btndescifrar").addEventListener("click", () => {
+    let toDecode = document.getElementById("message").value;
+    let offset = parseInt(document.getElementById("offset").value);
+    containerResult.innerHTML = cipher.decode(offset,toDecode);
+  //console.log("hola");
+
 });
 document.getElementById("resetbtn").addEventListener("click", () => {
   document.getElementById("message").value = "";
