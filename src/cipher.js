dbb45f5a820295d84e60ce86a8128ff741cf6883
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-02-14 08:24:24 -0600

    completed first part of cipher function that encrypt

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,23 @@
 let cipher = {
   encryptPassword: function(stringPassword, scrollNumber) {
-    let encryptResult = "";
-    for (let i = 0; i < stringPassword.lenght; i++) {
-      console.log("i", stringPassword[i]);
+    let encryptResult = '';
+    for (let i = 0; i < stringPassword.length; i++) {
       let codeCharacter = stringPassword.charCodeAt(i);
-      console.log("codeCharacter", codeCharacter)
-      let firstUppercaseNumber = 65;
-      let lastUppercaseNumber = 90;
+      let firstUppercaseNumberCode = 65;
+      let lastUppercaseNumberCode = 90;
       let totalCharacterAlphabetNumber = 26;
-      if (codeCharacter >= lastUppercaseNumber && codeCharacter <= lastUppercaseNumber) {
-       encryptResult += String.fromCharCode(codeCharacter - firstUppercaseNumber + scrollNumber) % totalCharacterAlphabetNumber + firstUppercaseNumber;
-       console.log("encrypt result", encryptResult)
+      if(codeCharacter >= firstUppercaseNumberCode && codeCharacter <= lastUppercaseNumberCode){
+          encryptResult += String.fromCharCode((codeCharacter - firstUppercaseNumberCode + scrollNumber) % totalCharacterAlphabetNumber + firstUppercaseNumberCode);
       }
     }
+    return encryptResult;
+
   },
   desencryptPassword: function(stringPassword, scrollNumber) {
-    console.log(stringPassword)
-    console.log(scrollNumber)
- }
+    var totalCharacterAlphabetNumber = 26;
+    scrollNumber = (totalCharacterAlphabetNumber - scrollNumber) % totalCharacterAlphabetNumber;
+    let result = cipher.encryptPassword(stringPassword, scrollNumber);
+    return result;
+  }
 };
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,49 @@
 // alias for import
 import Cipher from './cipher.js';
-// let inputPassword = ""
-// if (inputPassword.length == 0 || inputPassword.length == " ") {
-//   alert("No ingresaste tu contraseña");
-// }
 
-function showEncryptPage(str, num) {
-    console.log( Cipher.encryptPassword(str,num) );
-    document.getElementById("logins-second-page").style.display = "block";
+function showEncryptPage () {
+    document.getElementById("page-for-encrypt").style.display = "block";
     document.getElementById("logins-main-page").style.display = "none";
-    document.getElementById("logins-third-page").style.display = "none";
+    document.getElementById("page-for-desencrypt").style.display = "none";
+    document.getElementById("faqs_page").style.display = "none";
 }
 
-function showDesencryptPage(str, num) {
-    console.log( Cipher.desencryptPassword(str, num) );
+function showDesencryptPage () {
     document.getElementById("logins-main-page").style.display = "none";
-    document.getElementById("logins-second-page").style.display = "none";
-    document.getElementById("logins-third-page").style.display = "block";
+    document.getElementById("page-for-encrypt").style.display = "none";
+    document.getElementById("page-for-desencrypt").style.display = "block";
+    document.getElementById("faqs_page").style.display = "none";
 }
+
+function showFaqsPage () {
+    document.getElementById("logins-main-page").style.display = "none";
+    document.getElementById("page-for-encrypt").style.display = "none";
+    document.getElementById("page-for-desencrypt").style.display = "none";
+    document.getElementById("faqs_page").style.display = "block";
+}
+
+function returnMainPage () {
+    document.getElementById("logins-main-page").style.display = "block";
+    document.getElementById("page-for-encrypt").style.display = "none";
+    document.getElementById("page-for-desencrypt").style.display = "none";
+    document.getElementById("faqs_page").style.display = "none";
+}
+
+function valueToEncrypt () {
+    var scrollNumber = parseInt(document.getElementById("scroll-number-encrypt").value);
+    var stringPassword = document.getElementById('input-password-encrypt').value;
+    if (scrollNumber == 0 || stringPassword == "") {
+        alert("No ingrsaste contraseña o desplazamiento");
+    } else {
+        var result = Cipher.encryptPassword(stringPassword, scrollNumber);
+        document.getElementById("input-password-encrypt-result").innerText = result;
+    }
+  }
 
 window.pageEncrypt = showEncryptPage;
 window.pageDesencrypt = showDesencryptPage;
+window.faqsPage = showFaqsPage;
+window.returnMainPage = returnMainPage;
+window.valueToEncrypt = valueToEncrypt;
+
+  
