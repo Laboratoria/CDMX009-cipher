8b25d13ed05926a2fc8709c9c8542c8525c40c4b
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-02-13 18:41:56 -0600

    logré hacer el cifrado, ya me sale en la consola cambios guardados en cipher.js en el archivo ticketspersonalizados.html guardé div que me da el resultado al pulsar el (generar código)

--- src/cipher.js
+++ src/cipher.js
@@ -16,12 +16,14 @@ export default cipher;
 
 
 //PROBANDO CIFRAR
-
-let string = "COMPRAR BOLETOS"; //luego cambiar a textarea
-
-let offset = 5; //luego cambiar a select
 function cifrar() {
+    let string = document.getElementById("mensajeInput").value; //lo enlazo con mi id de textarea
+
+    let offset = document.getElementById("numerosCodificar").value; //lo enlazo con mi id de mi select el cual es el que contiene los números 
+
+
     let letras = string.split("");
+
     // EL SPLIT es un metodo por lo que siempre va a llevar paréntesis: split()
     // divide un String en un array.
     // me devuelve un array.
@@ -30,22 +32,23 @@ function cifrar() {
     let resultado = ""; //vendría siendo mi output
     letras.forEach(function(letra) { //forEach lo uso para recorrer cada uno de mis elemntos del array letras.
         codigo = letra.charCodeAt(); //en esta parte creé una variable de nombre código y uso el charCodeAt para convertir mis letras a su código ascii
-        console.log(codigo); //comprobando
+        var c = 0;
 
         if ((codigo >= 65) && (codigo <= 90)) { //aquí coloco el limite de mis letras al número al que pertenecen mayor o igual
-            c = String.fromCharCode(((codigo - 65 + offset) % 26) + 65); //ejecuto mi formula con variable de nombre c junto con el offset por ahora, para letras mayuscúlas
+            c = String.fromCharCode((((codigo - 65) + parseInt(offset)) % 26) + 65); //ejecuto mi formula con variable de nombre c junto con el offset por ahora, para letras mayuscúlas
 
         } else if ((codigo >= 97) && (codigo <= 122)) { //ejecuto mi formula junto con el offset por ahora, para letras minúsculas
-            c = String.fromCharCode(((codigo - 97 + offset) % 26) + 97);
+            c = String.fromCharCode((((codigo - 97) + parseInt(offset)) % 26) + 97);
 
         } else if (codigo == 32) {
             // c = String.fromCharCode(((codigo - 32 + offset) % 26) + 32);
             c = " "; //aquí le digo a mi código  que cuando haya un espacio lo respete y lo deje tal cuál
         }
-        console.log(c);
+
         resultado += c;
     });
-    console.log(resultado); //probando en consola
+    document.getElementById('resultadocifrado').innerHTML = "Tu código es: " + resultado;
+
 }
 
 //PROBANDO DESCIFRAR   
