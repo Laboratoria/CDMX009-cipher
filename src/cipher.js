8faa6d3c65956324f632025ce48a2bb0fd57896e
TangCherry <danaee16@gmail.com>
2020-02-14 13:43:29 -0600

    funcion cifra sin funcionamiento aun

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,20 @@
-    //variables
-let offset = document.getElementById("number"); //no. desplazamiento
-let text = document.getElementById("text"); //texto ingresado
-let newText = " "; // texto cifrado
-let newAscii = " ";//en ascii
-//let textEncode = " "; // texto cifrado
+const cipher = {encode, decode};
 
-    //cifrar
+   function encode (number, text ){
 
-   function cipher () { 
-    for (let i = 0; i < text.length; i++) {
-        newAscii = text.charCodeAt (i);
-    if (newAscii >= 65 && newAscii <=90);
-            let toCipher = (text - 65 + number) % 26 + 65;
-            newText = String.fromCharCode (toCipher);
-            //textEncode = textEncode (newText);
-         }
-         return newText;
+       let newText;
+       let toCipher;
+       let textEncode = " ";
+       //console.log (number,text);
+
+    for ( let i = 0; i < text.length; i++) {
+    let newText = text.charCodeAt (i);
+    if (newText >= 65 && newText <= 90);
+       let toCipher = (text - 65 + number) % 26 + 65;
+         textEncode = String.fromCharCode (toCipher);
+         result += textEncode;
+    }
+      return result;
    }
-export default cipher; 
 
+   export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import cipher from "cipher.js"; 
+import cipher from "./cipher.js"; 
 console.log(cipher);
 
 // boton para cifrar
-let text = document.getElementById ("text").value;
-let offset = parseInt (document.getElementById ("number").value);
-let newAscii = document.getElementById ("result").innerHTML;
-let text= document.getElementById("newText").value;
-document.getElementById("code").addEventListener ("click",text); 
+document.getElementById("code").addEventListener("click",function (){
+
+//function encode ( ) { 
+let textUser = document.getElementById ("text").value;
+let textUser1 = textUser.toUpperCase();
+let offset = document.getElementById ("number").value;
+let newWord = cipher.encode (offset, textUser1);
+//document.getElementById ("result").value=document.getElementById ("newWord");
+//let resultado = document.getElementById("result");
+//resultado.innerHTML= newWord;
+document.getElementById ("result").innerHTML= newWord + offset + textUser1;
+});
+
+
+
+//let resultado = document.getElementById("result");
+
 
 
 
