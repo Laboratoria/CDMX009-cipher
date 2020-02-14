37c0b4e84498686f1a509d3e0bf8a7795bc8bff8
akaren29 <akjassoj@outlook.com>
2020-02-13 22:58:48 -0600

    cambio de funcion

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,23 @@
 
-const cipher = {
-  encode: (offset, string) => {//offset es el número de desplazamiento
-    //string es el mensaje a cifrar
-  let codeCipher, position; //codecipher ->codigo ascci de letra 
-  //position ->posicion de la letra cifrada en el alfabeto
-  let messResult = '';//Resultado de mensaje descifrado
+function cipher () {
+  let myStringCipher = ''; //guardara el mensaje cifrado
+  let myString = message.value;
+  let offset = position.value; 
 
-   for (let i=0; i <string.length; i++){ //Recorrido de los caracteres del string
-     if(string[i] === ' '){ //Si el string tiene un espacio se deja igual
-        messResult += ' ';
-     }
-     else{
-       const codeAscii = string.charCodeAt(i);//Obtiene codigo Ascii de la letra
-         if(codeAscii > 90){
-           //Formula para Minusculas de 97-122
-           position = codeAscii - 97 + (offset % 26);
-           //Formula para obtener el codigo ascci en 97 empieza la a minuscula
-           codeCipher = (position % 26) + 97;
-           //Para Mayusculas de 65-90
-         }else{
-           position = codeAscii -65 + (offset % 26);
-           codeCipher = (position % 26) + 65;
-         }
-          const letterCipher = string.fromCharCode(codeCipher);
-          //Obtiene la letra que corresponde el codigo Ascii, ya cifrado
-          messResult += letterCipher;
-         }
-     }
-     return messResult;
+ for (let i=0; i < myString.length; i++){//Recorrerá el string
+ let codeAscii = myString.charCodeAt(i);//numero de letra Ascci ----->>ERROR 
+ let cipherFormula; //almacena formula
+ let theNewLetter; //almacena el valor de nueva letra cifrada
+
+   if (codeAscci >= 65 && codeAscii <= 90){ //Valor en Ascci
+    cipherFormula = ((codeAscci - 65) + parseInt (offset) % 26) + 65; //La formula obtiene nuevo numero de letra
+    theNewLetter = String.fromCharCode(cipherFormula); //obtiene el valor de letra cifrada
+    myStringCipher += theNewLetter; //Con la suma forma el string cifrado
+
    }
-  
+  }
+
+  return myStringCipher; //Retorna el valor de la cadena
 };
 
 export default cipher; 
