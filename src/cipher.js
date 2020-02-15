2563573ad1d9404fb20aad0bb96dcbe789bdf7a8
Margot <maria16ruiz81@gmail.com>
2020-02-15 14:59:35 -0600

    actualizacion de js

--- src/cipher.js
+++ src/cipher.js
@@ -9,15 +9,12 @@ export default cipher;
 function encode(keyNumber, mensaje) { 
 let textUp = mensaje.toUpperCase();
 let steps = Number(keyNumber);
-let textoCambiado = '';
   for(let i = 0; i<=textUp.length; i++){
     let textAscii = textUp.charCodeAt(i);
     let switchText = parseInt((textAscii - 65 + steps) % 26 + 65);
-    let secretText += String.fromCharCode(switchText);
-    return (secretText);
+    let texto = String.fromCharCode(switchText);
+    return (texto);
   }
-  textoCambiado += secretText;
-  return textoCambiado;
 }
 
 function decode(keyNumber, mensaje) { 
