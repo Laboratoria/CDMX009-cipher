332688008faee2e59bde73efe0708fb2baeb1901
Nallely <nallely232@hotmail.com>
2020-02-15 15:26:02 -0600

    Prueba

--- src/cipher.js
+++ src/cipher.js
@@ -1,81 +1,52 @@
-/*
+const cipher = {encode,decode};
+
 export default cipher;
-export default decipher;*/
 
-let secretkey3 = document.querySelector('#secretkey1');// almacena valor offset
-let secretkey4 = document.querySelector('#secretkey2');// almacena valor offset
-let textarea4 = document.querySelector('#textarea'); // almacena texto cifrado
-let textarea5 = document.querySelector('#textarea2');//almacena texto a Cifrar
-let textarea6 = document.querySelector('#textarea7'); // almacena texto descifrado
-let textarea9 = document.querySelector('#textarea8');//almacena texto a descifrar
+function encode(myString,offset){
+  var myStringCipher = '';
 
-
-
-document.getElementById("blissito1").onclick= function() {decipher()};
-
-/*FUNCION CIPHER*/
-
-function cipher(){
-  let myStringCipher = ''; // almacenar el valor del string Cifrado
-  let myString = textarea5.value;
-  let key1 = secretkey3.value;
-
-  for (var i = 0 ; i < myString.length ; i++){  // recorrer el string del usuario
-   let numberOfTheLetter = myString.charCodeAt(i); //numero de la letra en el codigo ASCII
-   let cipherFormula; //almacena fórmula de cifrado que se utiliza
-   let theNewLetter; // alamacena valor de la nueva letra cifrada
-   if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90) { // valor UNICODE de letras mayúsculas en ASCII
-     cipherFormula = (( (numberOfTheLetter - 65) + parseInt(key1)) % 26) + 65; // formula de Cifrado Cesar: obtener nuevo numero de letra  en el codigo ASCII
-     theNewLetter =  String.fromCharCode(cipherFormula); // obtener el valor de la letra cifrada
-     myStringCipher +=theNewLetter; // formar el string cifrado
-     textarea4.innerHTML =  myStringCipher;
-
-    } else if (numberOfTheLetter >= 97 && numberOfTheLetter <=122) { // valor UNICODE de letras minúsculas en ASCII
-     cipherFormula = ((( numberOfTheLetter - 97) + parseInt(key1)) % 26) + 97; // fórmula de cifrado Cesar
-     theNewLetter = String.fromCharCode(cipherFormula); // valor de letra cifrada
-     myStringCipher +=theNewLetter; // formar el string cifrado
-     textarea4.innerHTML =  myStringCipher;
-
-    }else if(numberOfTheLetter === 32){ // verificar si es un espacio vacio
-     myStringCipher += ' ';  // añadir espacio en string cifrado
+  for (var i = 0 ; i < myString.length ; i++){
+   var numberOfTheLetter = myString.charCodeAt(i);
+   var cipherFormula;
+   var theNewLetter;
+   if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90) {
+     cipherFormula = ( numberOfTheLetter - 65 + parseInt(offset)) % 26 + 65;
+     theNewLetter =  String.fromCharCode(cipherFormula);
+     myStringCipher +=theNewLetter;
+   } else if (numberOfTheLetter >= 97 && numberOfTheLetter <=122) {
+     cipherFormula = ( numberOfTheLetter - 97 + parseInt(offset)) % 26 + 97;
+     theNewLetter = String.fromCharCode(cipherFormula);
+     myStringCipher +=theNewLetter;
+   }else if(numberOfTheLetter === 32){
+     myStringCipher += ' ';
    }
    else {
-     break; // Si no es una letra detente
+     break;
     }
   }
-  return myStringCipher;   // Retorna el valor de la cadena cifrada
+  return myStringCipher;
 }
 
-/*FUNCION DECIPHER*/
 //funcion para descifrar una cadena de texto
-function decipher(){
-  let myStringDechiper = '';  //alamacenar el valor del string decifrado
-  let myString = textarea9.value;
-  let key2 = secretkey4.value;
-
-  for(var i = 0; i< myString.length ; i++){   //recorrer el string del usuario
-     let numberOfTheLetter = myString.charCodeAt(i);  //valor UNICODE de la letra en el código ASCII
-     let decipherFormula; //almacena fórmula de decifrado
-     let theNewLetter; // almacena el nuevo valor de la letra decifrada
-     if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90){  //saber si la letra está en mayúscula
-       decipherFormula = ((( numberOfTheLetter -13) - parseInt(key2) ) % 26) + 65; // formula para descifrar
-       theNewLetter = String.fromCharCode(decipherFormula); // obtener el valor de la letra descifrada
-       myStringDechiper +=theNewLetter; // formar la cadena descifrada
-       textarea6.innerHTML =  myStringDechiper;
-
-     }else if (numberOfTheLetter >= 97 && numberOfTheLetter <=122){ // saber si la letra está en minúscula
-       decipherFormula = (((numberOfTheLetter - 45) - parseInt(key2)) % 26) + 97; // formula para descifrar
-       theNewLetter = String.fromCharCode(decipherFormula); // obtener el valor de la letra descifrada
-       myStringDechiper +=theNewLetter; // formar la cadena descifrada
-       textarea6.innerHTML =  myStringDechiper;
-
-     } else if(numberOfTheLetter === 32){ // verificar si es un espacio vacio
-       myStringDechiper += ' '; // añadir un espacio en string decifrado
+function decode(myString1,offset1){
+  var myStringDechiper ='';
+  for(var i = 0; i< myString1.length ; i++){
+     var numberOfTheLetter = myString1.charCodeAt(i);
+     var decipherFormula;
+     var theNewLetter;
+     if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90){
+       decipherFormula = ( numberOfTheLetter -13 - parseInt(offset1)) % 26 + 65;
+       theNewLetter = String.fromCharCode(decipherFormula);
+       myStringDechiper +=theNewLetter;
+     }else if (numberOfTheLetter >= 97 && numberOfTheLetter <=122){
+       decipherFormula = ( numberOfTheLetter - 45 - parseInt(offset1)) % 26 + 97;
+       theNewLetter = String.fromCharCode(decipherFormula);
+       myStringDechiper +=theNewLetter;
+     } else if(numberOfTheLetter === 32){
+       myStringDechiper += ' ';
      } else {
        break;
      }
   }
-  return myStringDechiper;  // retornar el valor de la cadena decifrada
+  return myStringDechiper;
 }
-
-export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 import cipher from './cipher.js';
 
-document.getElementById("blissito").onclick=function() {cipher()};
+let offset = document.querySelector('#secretkey1');// almacena valor offset encode
+let offset1 = document.querySelector('#secretkey2');// almacena valor offset decode
+let myString = document.querySelector('#textarea2');//almacena texto a Cifrar
+let myString1 = document.querySelector('#textarea8');//almacena texto a descifrar
 
-//console.log(cipher);*/
-/*
-import cipher from './cipher.js';
-import decipher from './cipher.js';*/
+
+
+//CLICK CIFRADO
+function clickCifrado(){
+  let word = cipher.encode(myString.value, offset.value);
+  document.getElementById("textarea").innerText= word;
+
+}
+document.getElementById("blissito").onclick=function() {clickCifrado()};
+
+//CLICK DESCIFRADO
+
+function clickDescifrado(){
+  let word1 = cipher.decode(myString1.value, offset1.value);
+  document.getElementById("textarea7").innerText= word1;
+
+}
+document.getElementById("blissito1").onclick=function() {clickDescifrado()};
+
+//FUNCIONES DE PANTALLA 1
 
 function showAndHide (){
 
@@ -17,6 +36,7 @@ function showAndHide (){
 
 document.getElementById("boton").onclick = function() {showAndHide()};
 
+//FUNCIONES DE PANTALLA 2
 
 function showAndHide1 (){
 
