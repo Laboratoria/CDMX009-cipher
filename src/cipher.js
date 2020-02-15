d8e1ec8a92088c62916df7bdca1a01fa10178bd8
Margot <maria16ruiz81@gmail.com>
2020-02-15 06:45:18 -0600

    probando nuevo bucle

--- src/cipher.js
+++ src/cipher.js
@@ -5,46 +5,29 @@ const cipher = {
 
 export default cipher;
 
-//función: encode, argumentos: offset y mensaje  
+
 function encode(keyNumber, mensaje) { 
-//pasamos el mensaje a mayusculas
 let textUp = mensaje.toUpperCase();
-//pasamos el numero a valor numérico
 let steps = Number(keyNumber);
-/* creamos la variable i y pedimos que se siga ejecutando
-hasta que termine con el largo del argumento mensaje.*/
+let textoCambiado = '';
   for(let i = 0; i<=textUp.length; i++){
-//pedimos el valor ASCII de la letra
     let textAscii = textUp.charCodeAt(i);
-/*aplicamos el valor del argumento offset al valor ASCII
-con la formula de cifrado cesar*/
     let switchText = parseInt((textAscii - 65 + steps) % 26 + 65);
-/* creamos una variable que convierta de ASCII a letras
-y las una en una sola string */
-    let secretText = String.fromCharCode(switchText);
-    console.log encode(secretText);
+    let secretText += String.fromCharCode(switchText);
+    return (secretText);
   }
+  textoCambiado += secretText;
+  return textoCambiado;
 }
 
-//función: encode, argumentos: offset y mensaje  
 function decode(keyNumber, mensaje) { 
-  //pasamos el mensaje a mayusculas
   let textUp = mensaje.toUpperCase();
-  //pasamos el numero a valor numérico
   let steps = Number(keyNumber);
-  /* creamos la variable i y pedimos que se siga ejecutando
-  hasta que termine con el largo del argumento mensaje.*/
     for(let i = 0; i<=textUp.length; i++){
-  //pedimos el valor ASCII de la letra
       let textAscii = textUp.charCodeAt(i);
-  /*aplicamos el valor del argumento offset al valor ASCII
-  con la formula de cifrado cesar*/
-      let switchText = parseInt((textAscii - 65 + steps) % 26 + 65);
-  /* creamos una variable que convierta de ASCII a letras
-  y las una en una sola string */
+      let switchText = parseInt((textAscii - 65 - steps) % 26 + 65);
       let secretText = String.fromCharCode(switchText);
-      console.log decode(secretText);
+      return (secretText);
     }
   }
-  
   
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
-  console.log(cipher);
+
+let getBoton = document.getElementById("getBoton");
+getBoton.onclick = encode;
+
+function encode() {
+    let mensaje = document.getElementById("textBox").value;
+    let keyNumber = document.getElementById("numBox").value;
+    let texto = cipher.encode(keyNumber, mensaje); 
+    document.getElementById("demo").innerHTML = texto;
+}
+
+let letBoton = document.getElementById("letBoton");
+letBoton.onclick = decode;
+
+function decode() {
+    let mensaje = document.getElementById("textBox").value;
+    let keyNumber = document.getElementById("numBox").value;
+    let texto = cipher.decode(keyNumber, mensaje); 
+    document.getElementById("demo").innerHTML = texto;
+}
