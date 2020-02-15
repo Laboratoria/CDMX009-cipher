3031573045ae16627505313a690a5ccf7c280891
danleonca <danleon022688@gmail.com>
2020-02-14 20:55:35 -0600

    viernes 14 de feb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
-import cipher from './cipher.js';
 
+import cipher from './cipher.js';
 console.log(cipher);
+
+function cipherOne() {
+document.getElementById("FirstPage").style.display= "none";
+document.getElementById("SecondPage").style.display= "block";
+// me traigo el contenido de las cajas (string y offset)
+//let string = document.getElementById('texto').value;
+//console.log(string);
+//let space =document.getElementById('espacios').value;
+//console.log(space);
+let boxInput= document.getElementById('result_cipher');
+let string = document.getElementById('texto').value;
+console.log(string);
+let stringUpperCase1= string.toUpperCase();
+let space =document.getElementById('espacios').value;
+console.log(space);
+// le pido a cifer que haga su chamba
+let password =cipher.encode(stringUpperCase1, space);
+console.log(password);
+
+// lo coloco enm la nueva pantalla
+//document.getElementById("result_cipher").inner.HTML= password;
+boxInput.innerHTML= password;
+
+}
+
+
+function unCipher_one() {
+
+document.getElementById("FirstPage").style.display= "none";
+document.getElementById("ThirdPage").style.display= "block";
+
+let boxInput2= document.getElementById('Result');
+let string = document.getElementById('texto').value;
+console.log(string);
+let stringUpperCase= string.toUpperCase();
+
+let space =document.getElementById('espacios').value;
+console.log(space);
+
+let meaning =cipher.decode(stringUpperCase, space);
+console.log(meaning);
+boxInput2.innerHTML= meaning;
+ }
+
+function Back_one() {
+document.getElementById("FirstPage").style.display= "block";
+document.getElementById("SecondPage").style.display= "none";
+
+
+}
+function Back_oneSP() {
+document.getElementById("FirstPage").style.display= "block";
+document.getElementById("ThirdPage").style.display="none";
+}
+
+//let inPuttext= document.getElementById("texto");
+//let slicers= document.getElementById("espacios");
+//let inPuttextBox = inPuttext.value;
+//let slicersBox= slicers.value;
+
+//document.getElementById("result_cipher").innerHTML = cipher.encode(inPuttextBox, slicersBox);
+//document.getElementById("result_cipher").innerHTML = cipher.decode(inPuttextBox, slicersBox);
+
+//observador
+
+document.getElementById("FirstButton").addEventListener('click',cipherOne);
+document.getElementById("secondButton").addEventListener('click',unCipher_one);
+document.getElementById('BackToTheTop1').addEventListener('click', Back_one);
+document.getElementById('BackToTheTop1SP').addEventListener('click', Back_oneSP);
+
+
+
+////
+//document.getElementById("FirstButton").addEventListener("click", cipher.encode(inPuttextBox, slicersBox));
+//let inPuttext= document.getElementById("texto");
+//let slicers= document.getElementById("espacios");
+//
+//
+//document.getElementById("FirstButton").addEventListener("click", cipher());
+
+//document.getElementById("result_cipher").inner.HTML= cipher.results;
---
+++ src/pruebatwo.js
@@ -0,0 +1,107 @@
+////import cipher from './cipher.js';
+////console.log(cipher);
+//
+//function cipherOne() {
+//document.getElementById("FirstPage").style.display= "none";
+//document.getElementById("SecondPage").style.display= "block";
+////let resultEncodeCipher= document.getElementById("FirstButton").addEventListener("click", encode(inPuttextBox, slicersBox));
+////console.log(results);
+//
+////let cipherText = document.getElementById("FirstButton").onclick = function() {encode (inPuttextBox, slicersBox)};
+////document.getElementById("result_cipher").innerHTML = cipherText;
+//}
+//
+//function unCipher_one() {
+//document.getElementById("FirstPage").style.display= "none";
+//document.getElementById("ThirdPage").style.display= "block";
+// }
+//
+//function Back_one() {
+//document.getElementById("FirstPage").style.display= "block";
+//document.getElementById("SecondPage").style.display= "none";
+//
+//
+//}
+//function Back_oneSP() {
+//document.getElementById("FirstPage").style.display= "block";
+//document.getElementById("ThirdPage").style.display="none";
+//}
+//
+//let inPuttext= document.getElementById("texto");
+//let slicers= document.getElementById("espacios");
+//let inPuttextBox = inPuttext.value;
+//let slicersBox= slicers.value;
+
+
+
+
+
+
+//function cipherOne(){
+//
+//document.getElementById("FirstPage").style.display= "none";
+//document.getElementById("SecondPage").style.display= "block";
+//
+//}
+//
+//function unCipher_one() {
+//document.getElementById("FirstPage").style.display= "none";
+//document.getElementById("ThirdPage").style.display= "block";
+// }
+//
+//function Back_one() {
+//document.getElementById("FirstPage").style.display= "block";
+//document.getElementById("SecondPage").style.display= "none";
+//
+//
+//}
+//function Back_oneSP() {
+//document.getElementById("FirstPage").style.display= "block";
+//document.getElementById("ThirdPage").style.display="none";
+//}
+
+//function cipherOne() {
+//
+//document.getElementById("FirstPage").style.display= "none";
+//document.getElementById("SecondPage").style.display= "block";
+//
+//}
+//
+//function unCipher_one() {
+//document.getElementById("FirstPage").style.display= "none";
+//document.getElementById("ThirdPage").style.display= "block";
+// }
+//
+//function Back_one() {
+//document.getElementById("FirstPage").style.display= "block";
+//document.getElementById("SecondPage").style.display= "none";
+//document.getElementById("ThirdPage").style.display="none";
+//}
+
+//
+//
+//let inPuttext= document.getElementById("texto");
+//let slicers= document.getElementById("espacios");
+//
+//
+//document.getElementById("FirstButton").addEventListener("click", cipher());
+//
+//function cipher() {
+//  let results= '';
+//  let inPuttextBox = inPuttext.value;
+//  let slicersBox= slicers.value;
+//
+//  for (let i=0; i<inPuttextBox.length; i++1) {
+//    let position= inPuttext1.charCodeAt(i);
+//
+//    if( position >= 65 && position <=90) {
+//      let realPosition= ((position - 65 + parseInt(slicersBox) % 26 ) +65;
+//      let newLetter= (String.fromCharCode(realPosition);
+//      results+=newLetter;
+//  }
+//  //} if else {
+//  //  Breack; }
+//document.getElementById("result_cipher").inner.HTML= results;
+//  }
+//}
+//
