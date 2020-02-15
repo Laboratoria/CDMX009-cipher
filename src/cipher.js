f2e4a6e500254398034357706bc37a4c17955148
veroalrodriguez <veroalrodriguez@gmail>
2020-02-15 13:28:39 -0600

    Realicé primer funcionalidad al cipher, cambios en todas las demás carpetas

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,32 @@
 //};
 
 //export default cipher;
+
+window.cipher = {
+    //método otorgado en el readme
+    encode:(string, offset)=> {
+        //Creando variable para box de encode
+        let storyEncode = "";
+        //for para recorrer los caracteres de las palabras
+        for (let i = 0; i < string.length; i++) {
+            //variable que representa el código acsii de cada letra en el string
+            let character = string[i];
+            //condición para cifrar mayúsculas
+                if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                    //acumulando los caracteres cifrados por medio de la formula larga
+                    let storyCharacters = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                    //retornando el cifrado del string agregando String.fromCharCode
+                    storyEncode += String.fromCharCode(storyCharacters);
+                  //condición para cifrar minúsculas
+                } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                    //acumulando los caracteres cifrados por medio de la formula larga
+                    storyCharacters = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+                    //retornando el cifrado del string agregando String.fromCharCode
+                    storyEncode += String.fromCharCode(storyCharacters);
+                }
+             else {
+                storyEncode += character;
+            }
+        }
+        return storyEncode;
+    },
--- src/index.js
+++ src/index.js
@@ -2,26 +2,81 @@
 
 //console.log(cipher);
 
-    //Validando folio
-    let folio = document.getElementById("folio").value;
-        if (folio = H0G2NB){
-          return true}else{
-            alert("Folio incorrecto")
-          }
-    send.addEventListener("click", function(){
-      document.getElementById("login").style.display = 'none'
-    	document.getElementById("first-page").style.display = 'block'
 
-    })
-
-
-
-
-
-
-window.onload = () => {
-    /*Enlaces*/
-    const linkActive = document.getElementById("linkEncode");
-    const linkDesactive = document.getElementById("linkDecode");
-
+    //Cargando a Página de Instroducción (FOLIO)
+    function enter() {
+  let folio = document.getElementById("folio").value;
+  if (folio = "H0G2NB") {
+    document.getElementById("first-page").style.display = "block";
+    document.getElementById("login").style.display = "none";
+  } else {
+    alert("Folio incorrecto")
+  }
 }
+    //Link a Página de registro
+    function yes(){
+      document.getElementById("form").style.display = "block";
+      document.getElementById("login").style.display = "none";
+    }
+
+    //Icono a Página de Instroducción
+    //function send(){
+
+    //Leyenda al clickear enviar
+      function send(){
+        document.getElementById("name").value;
+        document.getElementById("email").value;
+        if ("name" === "" && "email" === ""){
+          document.getElementById("form").innerHTML="En breve nos pondremos en contacto contigo"
+        }
+    }
+    //Funcionalidad de logo: de registro a Login
+    function back1(){
+      document.getElementById("login").style.display = "block";
+      document.getElementById("reg").style.display = "none";
+    }
+
+    //Funcionalidad de logo: de first-page a login
+    function back(){
+      document.getElementById("login").style.display = "block";
+      document.getElementById("first-page").style.display = "none";
+    }
+
+    //Funcionalidad de Menú
+    function menu(){
+
+    }
+
+    //Página de herramienta
+  window.onload=()=>{
+      //Links
+      const linkOn = document.getElementById("link-encode");
+      const linkOff = document.getElementById("link-decode");
+
+      linkOn.addEventListener("click", function() {
+        document.getElementById("link-encode").classList.add("link-active");
+        document.getElementById("link-decode").classList.remove("link-active");
+        document.getElementById("box-encode").classList.remove("display_none");
+        document.getElementById("box-encode").classList.add("display_block");
+        document.getElementById("box-decode").classList.remove("display_block");
+        document.getElementById("box-decode").classList.add("display_none");
+    });
+
+      linkOff.addEventListener("click", function() {
+        document.getElementById("link-encode").classList.remove("link-active");
+        document.getElementById("link-decode").classList.add("link-active");
+        document.getElementById("msg-decode").classList.remove("display_none");
+        document.getElementById("msg-decode").classList.add("display_block");
+        document.getElementById("msg-encode").classList.remove("display_block");
+        document.getElementById("msg-encode").classList.add("display_none");
+    });
+
+  }
+
+  //Mensaje en box-ENCODE
+  const  buttonDecoder = document.getElementById("btn-encode");
+    buttonDecoder.addEventListener("click", function(){
+        let storyEncode = document.getElementById("box-msg-encode").value;
+        let numberOffset  =  document.getElementById("offset-e").value;
+        document.getElementById("encode-ready").innerHTML=window.cipher.encode(storyEncode, numberOffset);
+    });
