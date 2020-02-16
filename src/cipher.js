bbafabeb90197d0b830d71cc5a3afd9a1de3ae9d
EdithOrt <eoc.diseno@gmail.com>
2020-02-16 15:10:26 -0600

    ya me devulve un valor en data

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 const cipher = {
-    encode (desplazamiento, texto){
+    encode (texto, desplazamiento){
         let resultado= "";
-        for (let i=0;i<=texto.length;i++){
+        for (let i=0; i < texto.length; i++){
         let codificar= texto.charCodeAt(i);
         let parseado= parseInt(desplazamiento);
         let paso1= codificar-65+parseado;
@@ -9,7 +9,16 @@ const cipher = {
         let conversion= String.fromCharCode(paso2);
         resultado += conversion;
         return resultado;
-  }       }
-    
-};
+  }       },
+ decode (texto,desplazamiento){
+  let resultado2= "";
+    for(let i=0;i<=texto.length;i++){
+      let decodificar= texto.charCodeAt(i);
+      let parseado= parseInt(desplazamiento);
+      let paso1= decodificar+65-parseado;
+      let paso2= (paso1)%26+65;
+      let conversion= String.fromCharCode(paso2);
+      resultado2 +=conversion;
+        return resultado2;    
+    }}};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-//función para recoger datos de nombre de usuario
 import cipher from "./cipher.js";
 
-function recogeDatos() {
     //variable que conecta con DOM y recoge id de caja de texto
     let nombre= document.getElementById("cajanombre").value;
+    document.getElementById("siguiente").addEventListener('click',recogeDatos);
+    document.getElementById("cifrando").addEventListener('click', cifrar);
+    document.getElementById("descifrando").addEventListener('click', descifrar);
+
+//función para recoger datos de nombre de usuario
+function recogeDatos() {
      //Condicional para mandar alerta si no hay texto
-    if (nombre.length == " "){
+    /*if (nombre.length == " "){
         document.getElementById("advertencia").innerText = "Por favor poner tu nombre";
     //Condicional para mandar a segunda pantalla y dar bienvenida
-    } else {
+    } else {*/
         document.getElementById("pantallaUno").style.display="none";
         document.getElementById("pantallaDos").style.display="block";
         document.getElementById("bienvenido").innerText= "Bienvenid@ " + nombre;
     }
-}
-document.getElementById("botonSiguiente").addEventListener('click',recogeDatos);
 
 //Aquí comienza interacción para cifrar/descifrar
-let botonCifrar= document.getElementById("cifrando");
    function cifrar () {
-        let texto= document.getElementById("cajaMensaje");
-        let desplazamiento= document.getElementById("cajaClave");
-        let resultadoMensaje= cipher.encode(desplazamiento,texto);
+        let texto= document.getElementById("cajaMensaje").value;
+        let desplazamiento= document.getElementById("cajaClave").value;
+        let resultadoMensaje= cipher.encode(texto,desplazamiento);
         document.getElementById("cajaResultado").innerHTML=resultadoMensaje;      
 }
 
-botonCifrar.addEventListener('click', cifrar);
+    function descifrar () {
 
-recogeDatos();
-cifrar();
+    }
