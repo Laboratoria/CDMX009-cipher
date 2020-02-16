787bc41c360a3bb4b0196d5b4dd88ab3227c7f1b
paocmoras <paocmoras@hotmail.com>
2020-02-16 14:24:39 -0600

    agregando imagenes para el readme

--- src/index.js
+++ src/index.js
@@ -32,7 +32,6 @@ window.onload = () => {
     const btnTextEncode = document.getElementById("btn_encode");
     btnTextEncode.addEventListener("click", function() {
         let textEncode = document.getElementById("box-msg-encode").value;
-        // textEncode = textEncode.toUpperCase();
         let number_offset = document.getElementById("n_offset_e").value;
         document.getElementById("msg-encode-check").innerHTML = window.cipher.encode(textEncode, number_offset);
     });
@@ -41,7 +40,6 @@ window.onload = () => {
     const btnTextDecode = document.getElementById("btn_decode");
     btnTextDecode.addEventListener("click", function() {
         let textDecode = document.getElementById("box-msg-decode").value;
-        //textDecode = textDecode.toUpperCase();
         let number_offset = document.getElementById("n_offset_d").value;
         document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, number_offset);
     });
