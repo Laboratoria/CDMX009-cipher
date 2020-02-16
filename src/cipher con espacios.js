1035d2be4bcd7314b6c9e7c2855def0087eed49e
Margot <maria16ruiz81@gmail.com>
2020-02-16 13:59:48 -0600

    actualizacion css

---
+++ src/cipher con espacios.js
@@ -0,0 +1,41 @@
+const cipher = {
+  encode,
+  decode
+  };
+
+export default cipher;
+
+function encode(keyNumber, mensaje) { 
+let textUp = mensaje.toUpperCase();
+let steps = Number(keyNumber);
+let texto = '';
+  for(let i = 0; i<textUp.length; i++){
+    let textAscii = textUp.charCodeAt(i);
+    if (textAscii === 32) {
+      texto += String.fromCharCode(textAscii); 
+    }
+    else {
+    let switchText = parseInt(((textAscii + 65 + steps) % 26 )+ 65);
+    texto += String.fromCharCode(switchText);
+  }
+}
+  return texto;
+}
+
+function decode(keyNumber, mensaje) { 
+  let textUp = mensaje.toUpperCase();
+  let steps = Number(keyNumber);
+  let texto = '';
+    for(let i = 0; i<textUp.length; i++){
+      let textAscii = textUp.charCodeAt(i);
+      if (textAscii === 32) {
+        texto += String.fromCharCode(textAscii); 
+      }
+      else {
+      let switchText = parseInt(((textAscii + 65 - steps) % 26 )+ 65);
+      texto += String.fromCharCode(switchText);
+    }
+  }
+    return texto;
+  }
+  
--- src/index.js
+++ src/index.js
@@ -15,8 +15,23 @@ let letBoton = document.getElementById('letBoton');
 letBoton.onclick = decode;
 
 function decode() {
-    let mensaje = document.getElementById('textBox').value;
-    let keyNumber = document.getElementById('numBox').value;
+    let mensaje = document.getElementById('decodeBox').value;
+    let keyNumber = document.getElementById('numDeBox').value;
     let texto = cipher.decode(keyNumber, mensaje); 
-    document.getElementById('demo').innerHTML = texto;
+    document.getElementById('decodeDemo').innerHTML = texto;
 }
+
+let copyBoton = document.getElementById('copyBoton');
+copyBoton.onclick = copy;
+
+function copy() {
+    document.getElementById("demo").select();
+    document.execCommand("copy");
+}
+
+let refresh = document.getElementById('specsLogo');
+refresh.onclick = refreshPage;
+
+function refreshPage(){
+    window.location.reload();
+} 
