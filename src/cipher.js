59f1d975550e0fd739b3c597a05490826e821b02
Fernanda de la Peña ☆ <kapirupia@gmail.com>
2020-02-16 11:46:36 -0600

    CSS pag de código y JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,29 @@
+
 function cipher (offNum, nTxt) {
 
     //definir variables
   var nTxt = document.getElementById('nText').value;
   var i=0;
-  var finalTxt= " ";
+
   var offNum = parseInt(document.getElementById("offNumber").value);
 
     //conversión
 for (i=0; i<=nTxt.length; i++){
 
-  r=(nTxt.charCodeAt(i) -65 +offNum) %26 +65; //aplica la formula
-  var finalTxt= String.fromCharCode(r);
+    r=(nTxt.charCodeAt(i) -65 +offNum) %26 +65; //aplica la formula
+    var finalTxt= String.fromCharCode(r);
 
 console.log(finalTxt);
-//  document.write(finalTxt);
-document.getElementById("plantCode").innerHTML = offNum;
 }
+document.getElementById("demo").innerHTML = "Paragraph changed!";
 }
+
+/*function CodeWrite (){
+let plantCode = document.getElementById("plantCode").innerHTML;
+plantCode = finalTxt
+}
+CodeWrite ()//  document.write(finalTxt)
+}
+}*/
 
 // export default cipher;
