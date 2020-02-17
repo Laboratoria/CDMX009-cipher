8222f9c55128ea9de4830bb32256cc5a9885d73e
unknown <laura-a-c@hotmail.com>
2020-02-17 09:10:19 -0600

    hola

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import cipher from './cipher.js';
 
 console.log(cipher);
 
@@ -6,9 +5,30 @@ function getName() {
     let name=document .getElementById ("name").value;
     let pat=document.getElementById ("pat").value;
     let mat= document.getElementById ("mat").value;
-    console.log (name+ "" +pat+ "" +mat); 
+    console.log (name + "  " +  pat + "  " + mat); 
 }
 
 //listener u observador
 document.querySelector('#lac').addEventListener('click', getName)
 
+
+let ButtonEncode = document.getElementById("encode");
+let ButtonDecode = document.getElementById("decode");
+// se ponen 2 variables con lef para darle una funcion
+let string = '';
+let offset = 0;
+
+function actionData() {
+  string = document.getElementById("texto").value;
+  offset = parseInt(document.getElementById("displace").value);
+}
+
+ButtonEncode.onclick = function () {
+  actionData();
+  document.getElementById("result").value = cipher.encode(offset, string);
+};
+
+ButtonDecode.onclick = function () {
+  actionData();
+  document.getElementById("result").value = cipher.decode(offset, string);
+};
