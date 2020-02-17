0b7d021306eff8e833eecd1e302e7fb9f392cc2b
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-02-16 23:17:39 -0600

    last validation functions

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ console.log(cipher.decode(offset,inputText));
 
 console.log(cipher);*/
 
-
 let bluetoothSwitchStatus = document.getElementById("bluetoothSwitch");
 bluetoothSwitch(bluetoothSwitchStatus);
 bluetoothSwitchStatus.addEventListener("click", bluetoothSwitch);
@@ -26,11 +25,19 @@ let offsetStatus = document.getElementById("offset");
 offsetStatus.addEventListener("input", offsetValidate);
 
 let inputTextStatus = document.getElementById("inputTextarea");
-inputTextStatus.addEventListener("input", inputTextValidate);
+inputTextStatus.addEventListener("input", encodeDecode);
 
 let cleanButtonStatus = document.getElementById("cleanButton");
 cleanButtonStatus.addEventListener("click", cleanElements);
 
+let questionStatus = document.getElementById("question");
+questionStatus.addEventListener("mouseover", questionMsg);
+
+let infoStatus = document.getElementById("info");
+infoStatus.addEventListener("mouseover", infoMsg);
+
+
+/*
 function inputTextValidate(){
    
     if(/[^ a-zA-ZÁÉÍÓÚü]+/.test(inputTextStatus.value)){
@@ -44,16 +51,21 @@ function inputTextValidate(){
         encodeDecode(); 
     }
     encodeDecode(); 
-}
+}*/
 
 function bluetoothSwitch() {
+    
     if (bluetoothSwitchStatus.checked === true){
         console.log("ON");
         document.getElementById("contacts").disabled = false;
+        
     }else{
         console.log("OFF");
         let statusElement = document.getElementsByClassName("btStatus");
+        cleanElements();
         disabledElements(statusElement);
+        let contactsSelectStatus = document.getElementById("contacts");
+        contactsSelectStatus.value = "contactsTitle";
     }
     console.log(bluetoothSwitchStatus.checked);
 }
@@ -73,14 +85,21 @@ function contactsSelect() {
 
 function offsetValidate() {
     console.log(offsetStatus.value);
+    //let sendButtonStatus = document.getElementById("sendButton");
+    //sendButtonStatus.disabled = true;
+    console.log("paso disabled");
     if(isNaN(offsetStatus.value)){
-        let sendButtonStatus = document.getElementById("sendButton");
-        sendButtonStatus.disabled = true;
-        document.getElementById("offsetWarning").innerHTML = "El offset debe ser un número.";    
+        //sendButtonStatus.disabled = true;
+        document.getElementById("offset").style.borderColor = "red";
+        document.getElementById("warning").style.color = "red";
+        document.getElementById("warning").innerHTML = "El offset debe ser un número.";    
     }else{
-        document.getElementById("offsetWarning").innerHTML = "";
+        //sendButtonStatus.disabled = true;
+        document.getElementById("offset").style.borderColor = "";
+        document.getElementById("warning").innerHTML = "";
+        encodeDecode();
     }
-    encodeDecode();
+    
 }
 
 function cipherSelectedStatus() {
@@ -90,14 +109,15 @@ function cipherSelectedStatus() {
         console.log("Es code");
     }else if(decodeStatus.checked){
         console.log("Es decode");
-        
     }else{
         console.log("no es ninguna");
     }
-    encodeDecode();
+    offsetValidate();
+    //encodeDecode();
 }
 
 function encodeDecode(){
+    
     let encodeStatus = document.getElementById("encodeButton");
     let decodeStatus = document.getElementById("decodeButton");
     let offsetStatus = document.getElementById("offset");
@@ -106,22 +126,36 @@ function encodeDecode(){
     let inputTextStatus = document.getElementById("inputTextarea");
     let outputTextStatus = document.getElementById("outputTextarea");
     let inputText;
+    let sendButtonStatus = document.getElementById("sendButton");
+    sendButtonStatus.disabled = true;
     if (encodeStatus.checked && offsetStatus.value != ""){
         ableElements(statusElement);
+        sendButtonStatus.disabled = true;
         inputText = inputTextStatus.value;
         outputTextStatus.value = cipher.encode(offset,inputText);
         console.log(offset);
         console.log(cipher.encode(offset,inputText));
+        if(inputText != ""){
+            sendButtonStatus.disabled = false;
+            sendButtonStatus.addEventListener("click", cleanElements);
+            //sendButtonStatus.disabled = true;
+        }else{
+            console.log("esperando");
+        }
     }else if(decodeStatus.checked && offsetStatus.value != "") {
         ableElements(statusElement);
         let sendButtonStatus = document.getElementById("sendButton");
         sendButtonStatus.disabled = true;
-        let testText = "FTUF FT VO UFYUP EF QSVFCB";
-        inputTextStatus.value = testText;
+        //sendButtonStatus.disabled = true;
+        //let testText = "FTUF FT VO UFYUP EF QSVFCB";
+        //inputTextStatus.value = testText;
         inputText = inputTextStatus.value;
         outputTextStatus.value = cipher.decode(offset,inputText);
         console.log(cipher.decode(offset,inputText));
+    }else{
+        console.log("sale");
     }
+
 }
 
 function disabledElements(statusElement) {
@@ -156,9 +190,25 @@ function cleanElements() {
            // cleanElement[i].value = "contactsTitle";
         }else if(cleanElement[i].tagName == "TEXTAREA"){
             cleanElement[i].value = "";
+        }else if(cleanElement[i].tagName == "BUTTON"){
+            console.log("parece que fue button");
+            cleanElement[i].disabled = true;
         }else{
-            console.log("parece que fue button")
+            console.log("parece que fue select");
         }
     }
 
+}
+
+
+function questionMsg(){
+    document.getElementById("warning").innerHTML = "Elije a quien quieres mandar un mensaje."; 
+    document.getElementById("warning").style.color = "rgba(3,0,39,1)";
+    setTimeout(function(){ document.getElementById("warning").innerHTML = "" ; }, 3000);
+}
+
+function infoMsg(){
+    document.getElementById("warning").innerHTML = "Manda y recibe mensajes encriptados."; 
+    document.getElementById("warning").style.color = "rgba(3,0,39,1)";
+    setTimeout(function(){ document.getElementById("warning").innerHTML = "" ; }, 3000);
 }
