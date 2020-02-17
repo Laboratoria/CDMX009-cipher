28b514736c922e268f8bee3a5a58f541477e0125
Alissang <alitasang@gmail.com>
2020-02-17 10:46:13 -0600

    ultimos cambios cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,18 @@
 const cipher = {
+  code,
   decode
 };
 
-function decode(tex,offset){
+function code(tex,offset){
   let position
   var texConv="";
   for (let index = 0; index < tex.length; index++) {
-    let toConv= tex.charCodeAt(index);
-    
-
-    // let contador= [index];
-    
-    // aqui el if de los espacios
-    // if (toConv===32) {
-    //   let position= parseInt(toConv);
-    // } else {
-      // borrar hasta aqui
-  
+       let toConv= tex.charCodeAt(index);          
+       if (toConv==32) {
+           position= 32;
+    }
+    else {
+        
     position= parseInt(toConv)+ parseInt(offset);
   
      if (position >= 91) {
@@ -26,26 +22,45 @@ function decode(tex,offset){
     else{
       position= parseInt(toConv)+ parseInt (offset);
      } 
+    }
      texConv+= String.fromCharCode(position);
       
     }
     return texConv;
-
-    
-
-      // texConv= String.fromCharCode(position);
-      // document.write(texConv);
-     
+             
     }
-   
+
+
+// funcion decodificar
     
-  //  let texConv += contador;
-
-  //  contador= String.fromCharCode(position);
-  //  let texConv += contador;
-    
-
-
+function decode(tex,offset){
+  let positionD
+  var texConvD="";
+  for (let indexD = 0; indexD < tex.length; indexD++) {
+    let toConvD= tex.charCodeAt(indexD);
+            
+       if (toConvD==32) {
+      positionD= 32;
+    }
+    else {
+     
+    positionD= parseInt(toConvD)- parseInt(offset);
+  
+     if (positionD < 65) {
+      let xD=26;
+      positionD= positionD + xD;
+      }    
+    else{
+      positionD= parseInt(toConvD)- parseInt (offset);
+     } 
+    }
+     texConvD+= String.fromCharCode(positionD);
+      
+    }
+    return texConvD;
+             
+    }
+      
 
 export default cipher;
 
--- src/index.js
+++ src/index.js
@@ -3,22 +3,25 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 // refs
-let cifrarButton = document.getElementById('cifrar')
+let cifrarButton = document.getElementById('cifrar');
+let decifrarButton = document.getElementById('decifrar');
 
 function convert(){
   let tex= document.getElementById("text").value;
   let offset= document.getElementById("desplazamiento").value;
-  let result = cipher.decode(tex,offset);
+  let result = cipher.code(tex,offset);
   document.getElementById("textarea").innerHTML = result;
-  console.log(result);
-  // colocarla en el html
-
+ 
 }
 
-// function show() {
-//   let insert= document.getElementById("textarea").value;
-  
-// }
+function show() {
+  let tex= document.getElementById("text").value;
+  let offset= document.getElementById("desplazamiento").value;
+  let resultD = cipher.decode(tex,offset);
+  document.getElementById("textarea").innerHTML = resultD;
+
+ }
 
 // observadores
-cifrarButton.addEventListener('click', convert)
+cifrarButton.addEventListener('click', convert);
+decifrarButton.addEventListener('click', show);
