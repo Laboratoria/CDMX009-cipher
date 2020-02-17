bb252994111518869e53ddd753b383366b46ffe0
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-02-16 18:48:53 -0600

    Editar Readme

--- src/Docs/README.md
+++ src/Docs/README.md
@@ -1,11 +1,62 @@
-#Titulo
-Bullying School Chechenitza
+![image](https://user-images.githubusercontent.com/38740899/47084111-fd794080-d1e8-11e8-9d83-4ebde470cf6e.png)
 
-#Descripcion
+# Cifrado Cesar
 
-Ahora en la actualidad la mayoria de los niños traen celular, esta aplicación se creo porque tambien existe el bullying o amenazas
-en las escuelas y mas en lugar cerrados como en el salon o los sanitarios. Pueden enviar mensajes, sin ver que dicen exactamente.
+"Cifrado Cesar" es una app que permite cifrar y descifrar un mensaje con un numero de desplazamiento a elección.
+
+## Desarrollado para 
+
+[Laboratoria](http://laboratoria.la)
+
+## Tecnología usada
+
+* HTML5
+* CSS
+* Javascript ECMA6
+
+## Vistas
+
+### versión desktop
+
+![image](https://user-images.githubusercontent.com/38740899/47042484-bf850980-d161-11e8-96ea-3f61383fa37a.png)
+
+### versión mobile
+
+![image](https://user-images.githubusercontent.com/38740899/47042550-ee02e480-d161-11e8-85e3-5bc1dfa60883.png)
 
 
-  
-  
+## Intrucciónes de uso
+
+### Para Cifrar:
+
+* ingresar texto a cifrar.
+
+* elegir un offset de desplazamiento.
+
+* oprime boton "codifica tu mensaje".
+
+* recibe tu mensaje codificado.
+
+* oprime boton "limpiar" para refrescar y volver a utilizar.
+
+
+### Para Descifrar:
+
+* ingresar texto a descifrar.
+
+* elegir un offset de desplazamiento (el mismo que se utilizo para codificar).
+
+* oprime boton "decodifica tu mensaje".
+
+* recibe tu mensaje decodificado.
+
+* oprime boton "limpiar" para refrescar y volver a utilizar.
+
+## Enlace Deploy
+
+https://valepm0511.github.io/Cifrado-Cesar/
+
+
+
+
+
--- src/cipher.js
+++ src/cipher.js
@@ -6,11 +6,11 @@ window.cipher = {
             let caracteres = string[i];
             if (caracteres.match(/[a-z]/i)) {
                 if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-                    let texto = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-                    msg += String.fromCharCode(texto);
+                    let frase = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                    msg += String.fromCharCode(frase);
                 } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-                    texto = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
-                    msg += String.fromCharCode(texto);
+                    frase = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+                    msg += String.fromCharCode(frase);
                 }
             } else {
                 msg += caracteres;
@@ -30,12 +30,12 @@ window.cipher = {
                 console.log(string.charCodeAt(i));
 
                 if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-                    let texto = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
-                    msgdecode += String.fromCharCode(texto);
+                    let frase = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+                    msgdecode += String.fromCharCode(frase);
                     console.log("entra");
                 } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-                    let texto = ((string.charCodeAt(i) - 97 - parseInt(offset) + 52) % 26) + 97;
-                    msgdecode += String.fromCharCode(texto);
+                    let frase = ((string.charCodeAt(i) - 97 - parseInt(offset) + 52) % 26) + 97;
+                    msgdecode += String.fromCharCode(frase);
                 }
 
 
