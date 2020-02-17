377ac5720800e9e6a7a09d7e2fac8bd51aace8a4
Dianax69 <58241414+Dianax69@users.noreply.github.com>
2020-02-16 23:34:38 -0600

    final with comments

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,22 @@
-let cipher = {
+// defined variable cipher object
+let cipher = { 
+  // defined first propertie function with two parameters
   encryptPassword: function(stringPassword, scrollNumber) {
+    // created a string variable for save the result 
     let encryptResult = '';
+    // init a loop to know the string password length
     for (let i = 0; i < stringPassword.length; i++) {
+      // get the code (ASCII) for each letter 
       let codeCharacter = stringPassword.charCodeAt(i);
+      // defined variables for initialization (A) and final (Z)
       let firstUppercaseNumberCode = 65;
       let lastUppercaseNumberCode = 90;
       let totalCharacterAlphabetNumber = 26;
+      // created a conditional to set a range 
       if(codeCharacter >= firstUppercaseNumberCode && codeCharacter <= lastUppercaseNumberCode){
+          // increases the result in variable 
+          /* letter code in ASCCI - number of the letter in ASCII + scroll number
+              the total module of letters in alphabet + ASCII number of letter A in result 0 */ 
           encryptResult += String.fromCharCode((codeCharacter - firstUppercaseNumberCode + scrollNumber) % totalCharacterAlphabetNumber + firstUppercaseNumberCode);
       }
     }
