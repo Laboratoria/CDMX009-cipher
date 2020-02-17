bf6a81cf8ed079848471ddf8863219f1219da973
astridgutz <astridguierrezm@gmail.com>
2020-02-17 09:09:39 -0600

    Subiendo ultima version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let ButtonEncode = document.getElementById("encode");
+let ButtonDecode = document.getElementById("decode");
+let text ='';
+let displace = 0;
 
+function actionData(){
+    text =document.getElementById("text").value;
+    displace =parseInt(document.getElementById("displace").value);
+    }
 
- 
-
+ButtonEncode.onclick = function () {
+    actionData();
+    document.getElementById("result").value = cipher.encode(text, displace);
+      };
+  
+  ButtonDecode.onclick = function () {
+    actionData();
+    document.getElementById("result").value = cipher.decode(text, displace);
+       };
