f3b4c5dd8c9b55b3ef76cabe02810b505a4b80a6
lysisbe <lybgs87@yahoo.com.mx>
2020-02-18 09:19:31 -0600

    versión funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,39 @@ const cipher = {
 };
 
 export default cipher;
+
+const cipher = {
+  // ...
+};
+
+export default cipher;
+const cipher ={code,decode};
+
+function encode() {
+  let usuario = document.getElementById("nombre-example").value;
+  console.log (usuario.length);//es el valor de offset para mi variable//
+let cifrada ="";
+
+for (var i = 0 ; i < usuario.length ; i++){
+let asciiNum = str.charCodeAt(i);
+
+cipherWord = (asciiNum-65+parseInt(usuario))%26+65;
+
+cifrada += String.fromCharCode(cipherWord)
+
+}
+  return solved;
+
+function decode(){
+let descifrar ='';  //alamacenar el valor del string decifrado
+
+for (let i=0; i < usuario.length; i++) {
+
+let asciiNum = str.charCodeAt(i);
+
+decipherWord = (asciiNum +65 - parseInt(usuario))%26+ 65;
+
+descifrada += String.fromCharCode(decipherWord)
+
+ }
+return(solved);
