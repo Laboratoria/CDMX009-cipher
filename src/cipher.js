6413cfb5c4f821354a195918bbd749c42d0f090a
TangCherry <danaee16@gmail.com>
2020-02-18 11:40:08 -0600

    Pruebas 7/7 ya terminado y con el readme tambien

--- src/cipher.js
+++ src/cipher.js
@@ -6,36 +6,44 @@ const cipher = {encode, decode};
        let result = ""; 
        let toCipher;
        let textEncode = "";
-       //console.log (number,text);
-
+    
+   if (typeof text === "string") {
     for ( let i = 0; i < text.length; i++) {
      newText = text.charCodeAt (i);
-    if (newText >= 65 && newText <= 90) { 
-        toCipher = (newText - 65 + parseInt (number)) % 26 + 65;
-         textEncode = (String.fromCharCode (toCipher));
-         result += textEncode;
-      }
-    }
-      return result;
-   }
-     else { 
-        throw new TypeError ("Valor no válido");
-     }
-
-     function decode (number, text) {
-    let newText;
-    let result = "";
-    let toDicipher; 
-    let textDecode = "";
-
-     for (let i = 0; i < text.length; i++) { 
-   newText = text.charCodeAt (i);
       if (newText >= 65 && newText <= 90) { 
-     toDicipher = (newText + 65 - parseInt (number)) % 26 + 65;
-     textDecode = (String.fromCharCode (toDicipher));
-     result += textDecode;
-       }
-      }
+       toCipher = (newText - 65 + parseInt (number)) % 26 + 65;
+        textEncode = (String.fromCharCode (toCipher));
+         result += textEncode;
+      } //if formula
+    } //for
+      return result;
+   } //if typeof
+    else { 
+      throw new TypeError ("Valor no valido");
+    } //else
+     
+   } //funcion code
+
+   function decode (number, text) {
+
+       let newText;
+       let result = "";
+       let toDicipher; 
+       let textDecode = "";
+
+    if (typeof text === "string") {
+     for (let i = 0; i < text.length; i++) { 
+      newText = text.charCodeAt (i);
+       if (newText >= 65 && newText <= 90) { 
+       toDicipher = (newText + 65 - parseInt (number)) % 26 + 65;
+        textDecode = (String.fromCharCode (toDicipher));
+         result += textDecode;
+       } //if formula
+      }// for
          return result;
-     }
+    } //if typeof
+     else { 
+    throw new TypeError ("Valor no valido");
+     } //else
+   } //funcion
      export default cipher;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import cipher from "./cipher.js";
 // boton para cifrar
 document.getElementById("code").addEventListener("click",function (){
 
-//function encode ( ) { 
-let textUser = document.getElementById ("text").value;
-let textUser1 = textUser.toUpperCase();
-let offset = document.getElementById ("number").value;
-let newWord = cipher.encode (offset, textUser1);
+//function encode
+    let textUser = document.getElementById ("text").value;
+    let textUser1 = textUser.toUpperCase();
+    let offset = document.getElementById ("number").value;
+    let newWord = cipher.encode (offset, textUser1);
 //document.getElementById ("result").value=document.getElementById ("newWord");
 //let resultado = document.getElementById("result");
 //resultado.innerHTML= newWord;
@@ -19,10 +19,10 @@ document.getElementById ("result").innerHTML= newWord;
 document.getElementById ("decode").addEventListener ("click",function () {
 
 //funcion decode 
-let textUser = document.getElementById ("text").value;
-let textUser1 = textUser.toUpperCase ();
-let offset = document.getElementById ("number").value;
-let newWord = cipher.decode (offset, textUser1);
+    let textUser = document.getElementById ("text").value;
+    let textUser1 = textUser.toUpperCase ();
+    let offset = document.getElementById ("number").value;
+    let newWord = cipher.decode (offset, textUser1);
 
 document.getElementById ("result").innerHTML = newWord;
 });
