a8c2a6f756e5d5f70a1031c39db5436169ce534c
lysisbe <lybgs87@yahoo.com.mx>
2020-02-18 16:54:58 -0600

    Arreglar orden de parámetros, agregar TypeError, corregir formula para decode

--- src/cipher.js
+++ src/cipher.js
@@ -4,32 +4,38 @@ const cipher = {
 };
 export default cipher;
 
-function encode(txtToEncode, offset) {
+function encode(offset, txtToEncode) {
+  txtToEncode = String(txtToEncode);
+  if(txtToEncode.length < 1) {
+    throw new TypeError("Texto a cifrar está vacío");
+  }
   let txtEncoded = "";
   for (let i = 0; i < txtToEncode.length; i++) {
     let asciiNum = txtToEncode.charCodeAt(i);
     if (asciiNum >= 65 && asciiNum <= 90){
-      let asciiNumEncoded = (asciiNum-65+parseInt(offset))%26+65;
-      txtEncoded += String.fromCharCode(asciiNumEncoded); 
+      let asciiNumEncoded = (((asciiNum - 65) + parseInt(offset)) % 26) + 65;
+      txtEncoded += String.fromCharCode(asciiNumEncoded);
     } else {
-      console.log("Error: Sólo se aceptan mayúsculas");
-      throw new TypeError;
+      throw new TypeError("Sólo se aceptan letras mayúsculas");
     }
   }
 
   return(txtEncoded);
 }
 
-function decode(txtToDecode, offset) {
+function decode(offset, txtToDecode) {
+  txtToDecode = String(txtToDecode);
+  if(txtToDecode.length < 1) {
+    throw new TypeError("Texto a descifrar está vacío");
+  }
   let txtDecoded = "";
   for (let i = 0; i < txtToDecode.length; i++){
     let asciiNum = txtToDecode.charCodeAt(i);
     if (asciiNum >= 65 && asciiNum <= 90){
-      let asciiNumDecoded = (asciiNum-65+parseInt(offset))%26+65;
-      txtDecoded += String.fromCharCode(asciiNumDecoded); 
+      let asciiNumDecoded = (((asciiNum + 65) - parseInt(offset)) % 26) + 65;
+      txtDecoded += String.fromCharCode(asciiNumDecoded);
     } else {
-      console.log("Error: Sólo se aceptan mayúsculas");
-      throw new TypeError;
+      throw new TypeError("Error: Sólo se aceptan mayúsculas");
     }
   }
 
