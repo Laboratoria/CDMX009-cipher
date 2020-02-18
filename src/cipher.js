60d53362193074ed6095c9ed56aa70fd86ccf23b
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-02-17 23:26:24 -0600

    cambios en indexjs

--- src/cipher.js
+++ src/cipher.js
@@ -1,70 +1,44 @@
 const cipher = {
-  encode,
-  decode,
-}
 
-function encode (offset, text) {
-  //variables
-  let textToAscii = 0;
-  let cipherText = [];
-  let formula = 0;
-  let result = 0;
-//ciclo para asignar el numero de letras
+encode (offset, text) {
+  let cipherText;
+  let encodeFinal = '';
+
   for (let i=0; i < text.length; i++) {
-    textToAscii = parseInt(text.charAt(i).charCodeAt (0));
-//ciclo que va a limitar ascii a mayusculas y dar texto cifrado
+    let mayusText = text.toUpperCase();
+    let textToAscii = mayusText.charCodeAt(i);
     if (textToAscii >= 65 && textToAscii <= 90) {
-      formula = (textToAscii-65+offset)%26+65;
-      cipherText[i] = String.fromCharCode(formula);
-      result += cipherText[i]+'';
-      //document.write (cipherText);
-    }
- //ciclo que va a dar los espacios donde corresponden   
-    //else if (textToAscii === 32) {
-      //let space = String.fromCharCode(textToAscii);
-      //document.write(space);
-    //}
-    //if (textToAscii >= 97 && textToAscii <= 122) {
-    //  let formula = (textToAscii-97+offset)%26+97;
-    //  let cipherText = String.fromCharCode(formula);
-    //  //document.write (cipherText);
-    //}
-  }
-  return result;
-
-}
-
-
-function decode (offset, text) {
-  //variables
-  let textToAscii = 0;
-  let cipherText = [];
-  let formula = 0;
-  let result = 0;
-  //ciclo para asignar el numero de letras
-    for (let i=0; i < text.length; i++) {
-      textToAscii = parseInt(text.charAt(i).charCodeAt (0));
-  //ciclo que va a limitar ascii a mayusculas y dar texto descifrado
-      if (textToAscii >= 65 && textToAscii <= 90) {
-        formula = (textToAscii+65-offset)%26+65;
+        let formula = (textToAscii-65+offset)%26+65;
         cipherText = String.fromCharCode(formula);
-        result += cipherText[i]+'';
-      }
-   //ciclo que va a dar los espacios donde corresponden   
-      //else if (textToAscii === 32) {
-      //  let space = String.fromCharCode(textToAscii);
-      //  document.write(space);
-      //}
-      //if (textToAscii >= 97 && textToAscii <= 122) {
-      //  let formula = (textToAscii-97-offset)%26+97;
-      //  let cipherText = String.fromCharCode(formula);
-      //  document.write (cipherText);
-      //}
-    }
-    return result;
-}
+        }
+       else if (textToAscii === 32) {
+       cipherText = String.fromCharCode(textToAscii);
+       }
+    encodeFinal += String.fromCharCode(cipherText);
+  }
+  return encodeFinal;
+}  
+,decode (offset, text) {
+  let decText;
+  let decodeFinal = '';
+
+  for (let i=0; i < text.length; i++) {
+    let mayusText = text.toUpperCase();
+    let textToAscii = mayusText.charCodeAt(i);
+    if (textToAscii >= 65 && textToAscii <= 90) {
+        let formula = (textToAscii-65+offset)%26+65;
+        decText = String.fromCharCode(formula);
+        }
+       else if (textToAscii === 32) {
+       decText = String.fromCharCode(textToAscii);
+       }
+    decodeFinal += String.fromCharCode(decText);
+  }
+  return decodeFinal;
+}  
 
 
+}; 
 export default cipher;
 
 ////////////////////////////////////////////////////////////////77
--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
 import cipher from './cipher.js';
 
+
+let encButton = document.getElementById('encodeButton');
+encButton.addEventListener('click', encodeButton);
+
+function encodeButton() {
+    let text = document.getElementById('textToEncode').value;
+    let offset = document.getElementById('offsetEncode').value;
+    let result = cipher.encode(offset,text);
+    document.getElementById('encodeText').innerHTML = result;
+}
+
+let decButton = document.getElementById('decodeButton');
+decButton.addEventListener('click', decodeButton);
+function decodeButton() {
+    let text = document.getElementById('textToDecode').value;
+    let offset = document.getElementById('offsetDecode').value;
+    let result = cipher.decode(offset,text);
+    document.getElementById('decodeText').innerHTML = result;    
+}
+
+//console.log(cipher);
 //variables que van a guardar los botones
 //let encodeButton = document.getElementById('encodeButton');
 //encodeButton.addEventListener ('click',
 //funcion para el boton de cifrado e imprimir resultado
-function eButton () {
-    let encodeButton = document.querySelector('#encodeButton');
-    let num = document.getElementById('offsetEncode');
-    let offset = (num.value); 
-    let string = document.querySelector('textToEncode');
-    let text = (string.value);
-    let cifText = cipher.encode(offset, text);
-    //let encodeText = document.getElementById('encodeText');
+//function eButton () {
+//    let encodeButton = document.querySelector('#encodeButton');
+//    let num = document.getElementById('offsetEncode');
+//    let offset = (num.value); 
+//    let string = document.querySelector('textToEncode');
+//    let text = (string.value);
+//    let cifText = cipher.encode(offset, text);
+//    //let encodeText = document.getElementById('encodeText');
     //encodeText.innerHTML = cifText;
-    document.querySelector('#encodeText').value=cifText;
-    encodeButton.addEventListener('click',eButton);
-};
+//    document.querySelector('#encodeText').value=cifText;
+//    encodeButton.addEventListener('click',eButton);
+//};
 
 //variables que van a guardar los botones
 //let decodeButton = document.getElementById('decodeButton');
 //decodeButton.addEventListener ('click',
 //funcion para el boton de descifrado e imprimir resultado
-function dButton () {
-    let decodeButton = document.querySelector('decodeButton');
-    let num = document.getElementById('offsetDecode');
-    let offset = (num.value); 
-    let string = document.getElementById('textToDecode').value;
-    let text = (string.value);
-    let decText = cipher.decode(offset, text);
+//function dButton () {
+//    let decodeButton = document.querySelector('decodeButton');
+//    let num = document.getElementById('offsetDecode');
+//    let offset = (num.value); 
+//    let string = document.getElementById('textToDecode').value;
+//    let text = (string.value);
+//    let decText = cipher.decode(offset, text);
     //let decodeText = document.getElementById('decodeText').value;
     //decodeText.innerHTML = decText;
-    document.querySelector('decodeText').value=decText;
-    decodeButton.addEventListener('click',dButton);
-};
+//    document.querySelector('decodeText').value=decText;
+//    decodeButton.addEventListener('click',dButton);
+//};
 ///////////////////////////////////////
 //encodeButton.onclick= function pressEncode () {
 //    let text = document.getElementById('textToEncode').value;
@@ -42,4 +63,4 @@ function dButton () {
 //}
 
 
-console.log(cipher);
+
