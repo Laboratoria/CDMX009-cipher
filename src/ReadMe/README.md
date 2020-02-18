44d2978a7e92b496f60a856238952c0e58e60949
danleonca <danleon022688@gmail.com>
2020-02-18 11:04:04 -0600

    martes

--- src/ReadMe/README.md
+++ src/ReadMe/README.md
@@ -16,6 +16,64 @@ Mi Prototipo final lo hice en figma con el sig link pueden acceder a el:
 
 https://www.figma.com/file/QkViAyb14FaHSmMHfeEh7a/Untitled?node-id=0%3A1
 
+# Objetivos de aprendizaje
+
+
+
+### UX
+
+- [x ] Diseñar la aplicación pensando y entendiendo al usuario.
+- [ X] Crear prototipos para obtener feedback e iterar.
+- [X ] Aplicar los principios de diseño visual (contraste, alineación, jerarquía).
+
+### HTML y CSS
+
+- [ x] Uso correcto de HTML semántico.
+- [X ] Uso de selectores de CSS.
+- [X ] Construir tu aplicación respetando el diseño realizado (maquetación).
+
+### DOM
+
+- [ X] Uso de selectores del DOM.
+- [X ] Manejo de eventos del DOM.
+- [ X] Manipulación dinámica del DOM.
+
+### Javascript
+
+- [ X] Manipulación de strings.
+- [ /] Uso de condicionales (if-else | switch).
+- [ /] Uso de bucles (for | do-while).    
+- [ /] Uso de funciones (parámetros | argumentos | valor de retorno).
+- [X ] Declaración correcta de variables (const & let).
+
+### Testing
+- [X ] Testeo de tus funciones.
+
+### Git y GitHub
+- [X ] Comandos de git (add | commit | pull | status | push).
+- [X ] Manejo de repositorios de GitHub (clone | fork | gh-pages).
+
+### Buenas prácticas de desarrollo
+- [ /] Uso de identificadores descriptivos (Nomenclatura | Semántica).
+- [ X] Uso de linter para seguir buenas prácticas (ESLINT).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/cipher.js
+++ src/cipher.js
@@ -2,36 +2,39 @@ const cipher = {encode, decode};
 
 
 function encode (space, world) {
-  let results= '';
+let results= '';
 let newLetter;
 let realPosition;
-var letters = /^[A-Z]+$/;
-if (world.match(letters)){
-//if (world.match(letters)) {
+let letters = /^[A-Z]+$/;
+//if (typeof world == "string"){
+if (world.match(letters)) {
   for (let i=0; i<world.length; i++) {
     let position= world.charCodeAt(i);
 
 if ( position >= 65 && position <=90) {
-  realPosition = (((position - 65 + parseInt(space)) % 26 ) +65);
-  newLetter= (String.fromCharCode(realPosition));
-  results+=newLetter;
+    realPosition = (((position - 65 + parseInt(space)) % 26 ) +65);
+    newLetter= (String.fromCharCode(realPosition));
+    results+=newLetter;
 
 
 
-   }
- }
-        return results;
       }
     }
 
-
+       return results;
+//      } else {
+//        throw new TypeError("sólo letras");
+//
+//
+  }
+}
 
 
 function decode (spaceDecode, worldDecode) {
   let resultsD= '';
   let realPositionD;
   let newLetterD;
-  var letters = /^[A-Z]+$/;
+  let letters = /^[A-Z]+$/;
   if (worldDecode.match(letters)) {
     for (let i=0; i<worldDecode.length; i++) {
       let positionD= worldDecode.charCodeAt(i);
@@ -40,8 +43,9 @@ function decode (spaceDecode, worldDecode) {
         realPositionD = (((positionD + 65 - parseInt(spaceDecode)) % 26 ) +65);
         newLetterD= (String.fromCharCode(realPositionD));
         resultsD+=newLetterD;
-      }
 
+
+}
     }
 
     return resultsD;
