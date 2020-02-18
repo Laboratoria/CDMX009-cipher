dc4e8f52e19d7d44f87467e1d5879dfcc3f5e378
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-02-18 13:39:01 -0600

    modificaciones en botones y en formulas

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,70 @@
 const cipher = {
+  encode,
+  decode,
+}
 
-encode (offset, text) {
-  let cipherText;
-  let encodeFinal = '';
-
+function encode (offset, text) {
+  //variables
+  let textToAscii = 0;
+  let cipherText = [];
+  let formula = 0;
+  let result = 0;
+//ciclo para asignar el numero de letras
   for (let i=0; i < text.length; i++) {
-    let mayusText = text.toUpperCase();
-    let textToAscii = mayusText.charCodeAt(i);
+    textToAscii = parseInt(text.charAt(i).charCodeAt (0));
+//ciclo que va a limitar ascii a mayusculas y dar texto cifrado
     if (textToAscii >= 65 && textToAscii <= 90) {
-        let formula = (textToAscii-65+offset)%26+65;
+      formula = (textToAscii-65+offset)%26+65;
+      cipherText[i] = String.fromCharCode(formula);
+      result += cipherText[i]+'';
+      //document.write (cipherText);
+    }
+ //ciclo que va a dar los espacios donde corresponden   
+    //else if (textToAscii === 32) {
+      //let space = String.fromCharCode(textToAscii);
+      //document.write(space);
+    //}
+    //if (textToAscii >= 97 && textToAscii <= 122) {
+    //  let formula = (textToAscii-97+offset)%26+97;
+    //  let cipherText = String.fromCharCode(formula);
+    //  //document.write (cipherText);
+    //}
+  }
+  return result;
+
+}
+
+
+function decode (offset, text) {
+  //variables
+  let textToAscii = 0;
+  let cipherText = [];
+  let formula = 0;
+  let result = 0;
+  //ciclo para asignar el numero de letras
+    for (let i=0; i < text.length; i++) {
+      textToAscii = parseInt(text.charAt(i).charCodeAt (0));
+  //ciclo que va a limitar ascii a mayusculas y dar texto descifrado
+      if (textToAscii >= 65 && textToAscii <= 90) {
+        formula = (textToAscii+65-offset)%26+65;
         cipherText = String.fromCharCode(formula);
-        }
-       else if (textToAscii === 32) {
-       cipherText = String.fromCharCode(textToAscii);
-       }
-    encodeFinal += String.fromCharCode(cipherText);
-  }
-  return encodeFinal;
-}  
-,decode (offset, text) {
-  let decText;
-  let decodeFinal = '';
-
-  for (let i=0; i < text.length; i++) {
-    let mayusText = text.toUpperCase();
-    let textToAscii = mayusText.charCodeAt(i);
-    if (textToAscii >= 65 && textToAscii <= 90) {
-        let formula = (textToAscii-65+offset)%26+65;
-        decText = String.fromCharCode(formula);
-        }
-       else if (textToAscii === 32) {
-       decText = String.fromCharCode(textToAscii);
-       }
-    decodeFinal += String.fromCharCode(decText);
-  }
-  return decodeFinal;
-}  
+        result += cipherText[i]+'';
+      }
+   //ciclo que va a dar los espacios donde corresponden   
+      //else if (textToAscii === 32) {
+      //  let space = String.fromCharCode(textToAscii);
+      //  document.write(space);
+      //}
+      //if (textToAscii >= 97 && textToAscii <= 122) {
+      //  let formula = (textToAscii-97-offset)%26+97;
+      //  let cipherText = String.fromCharCode(formula);
+      //  document.write (cipherText);
+      //}
+    }
+    return result;
+}
 
 
-}; 
 export default cipher;
 
 ////////////////////////////////////////////////////////////////77
