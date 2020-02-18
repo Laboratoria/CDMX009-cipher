159ecd6aa56839e17a2bdac91ea3194605a93e0e
Laboratoria <adrianagrisss@gmail.com>
2020-02-18 09:58:30 -0600

    Mi actualización

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,16 @@ const cipher = {
 };
 
 export default cipher;
+
+
+function codigoCifrado (mensaje,desplazamiento) {
+  let resultado = " ";
+let mensaje = "ANA";
+let desplazamiento= "5";
+for (let i = 0; i < mensaje.length; i++) {
+  let textoMayuscula= mensaje.toUpperCase();
+  let codigoAsc= (textoMayuscula.charCodeAt(i) - 65 + parseInt(desplazamiento)) % 26 + 65;
+  resultado += String.fromCharCode(codigoAsc);
+  console.log(resultado);
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+
+
+
+
+function segundapantalla() {
+    let pantallas = document.getElementById("pantallaUno")
+    pantallas.style = "display:none";
+    document.getElementById("pantallaDos").style.display = "block";
+}
+
+
+document.getElementById('ir').addEventListener('click', segundapantalla);
+
