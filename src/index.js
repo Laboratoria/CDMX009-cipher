00823967cbd62b1392052d23fb3e16598d671837
JimenaMV <jimenamanzanares.v@gmail.com>
2020-02-18 13:48:28 -0600

    avance con funcionalidad de los botones delas pantallas

--- src/index.js
+++ src/index.js
@@ -1,42 +1,73 @@
 import cipher from './cipher.js';
 
+//
+let firstScreen = document.getElementById("first-screen");
+let secondScreen = document.getElementById("second-screen");
+let thirdScreen = document.getElementById("third-screen");
+let str = document.getElementById("inputString");
+
 // Funciones para el cambio de pantallas
 
-document.getElementById("change").addEventListener("click", segundapantalla);
-
-function segundapantalla() {
-    let pantallas = document.getElementById("first-screen");
-    pantallas.style = "display:none"; //El valor display: none le dice al navegador que no lo represente; el resultado es que el elemento queda escondido 
-    document.getElementById("third-screen").style.display = "block";
+function segundaPantalla() {
+    firstScreen.style="display:none";
+    secondScreen.style="display:block";
+    thirdScreen.style="display:none";
 }
+
+function terceraPantalla() {
+    firstScreen.style="display:none";
+    secondScreen.style="display:none";
+    thirdScreen.style="display:block";
+} 
+
+function primeraPantalla() {
+    firstScreen.style="display:block";
+    secondScreen.style="display:none";
+    thirdScreen.style="display:none";
+} 
+
+/*
+function clickCifrar () {
+ let resultado = cipher.encode (str.value, 2)
+}
+ */
+
+//Listeners
+document.getElementById("change").addEventListener("click", segundaPantalla);
+document.getElementById("change2").addEventListener("click", terceraPantalla);
+document.getElementById("chinampalogo").addEventListener("click", primeraPantalla);
+
+//document.getElementById("2").addEventListener("click", clickCifrar);
+
+
+console.log (offset);
+
 /*
 function segundapantalla () {
     document.querySelector ("#second-screen").style.display = "block";
     document.querySelector ("#first-screen").style.display = "none";
 
 };
-*/
-// Funciones para botones
+
+Funciones para botones
+let productos = document.querySelectorAll(".productos");
 let buttonEncode = document.getElementById ("buttonEncode");
 let buttonDecode = document.getElementById("buttonDecode");
 
-buttonEncode.addEventListener("click", function(){
+buttonEncode.addEventListener("click", () =>{
     let str = document.getElementById("inputString").value;
     let offset = document.getElementById("productos").value;
     let codeWord = cipher.encode (str,offset);
     document.getElementById ("solved").innerHTML = codeWord;
 
-buttonDecode.addEventListener("click", function(){
+buttonDecode.addEventListener("click", () =>{
     let str = document.getElementById("inputString").value;
-    let offset = document.getElementById("productos").value;
+    let offset = document.getElementById("offset").value;
     let codeWord = cipher.encode (str,offset);
     document.getElementById ("solved").innerHTML = codeWord;
 
-
-//globales let offset
 
 // refs
-let productos = document.querySelectorAll('.productos')
 
 // funciones
 function recibeClick(evento){
@@ -56,3 +87,4 @@ productos[2].addEventListener('click',recibeClick)
 productos[3].addEventListener('click',recibeClick)
 
 console.log(cipher);
+*/
