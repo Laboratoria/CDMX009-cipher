4c5b953fe4235ee09cd8e7698e1e4c8ade646d3e
Ely <60673917+ElizaFlores11@users.noreply.github.com>
2020-02-18 09:43:28 -0600

    Desactivar Mayuscykas
    Desactivar el cambio de mayusculas

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ window.onload = () => {
 	const btnTextEncode = document.getElementById("btn_encode");
     btnTextEncode.addEventListener("click", function() {
 	    let textEncode = document.getElementById("box-msg-encode").value;
-	     textEncode = textEncode.toUpperCase();
+	     //textEncode = textEncode.toUpperCase();
 	    let number_offset = document.getElementById("numero-encode").value;
 	    document.getElementById("resultado-cifrado").innerHTML = window.cipher.encode(textEncode, number_offset);
     });
@@ -45,7 +45,7 @@ window.onload = () => {
     const btnTextDecode = document.getElementById("btn_decode");
     btnTextDecode.addEventListener("click", function() {
         let textDecode = document.getElementById("box-msg-decode").value;
-        textDecode = textDecode.toUpperCase();
+        //textDecode = textDecode.toUpperCase();
         let number_offset = document.getElementById("numero-decode").value;
         document.getElementById("respuesta-decode").innerHTML = window.cipher.decode(textDecode, number_offset);
     });
