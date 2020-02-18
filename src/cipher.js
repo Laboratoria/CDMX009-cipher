034cedb24bf4b83ab20120ac5313b05a34bf4516
EdithOrt <eoc.diseno@gmail.com>
2020-02-17 19:35:39 -0600

    Ya quedaron condicionales js :D

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,30 @@
 const cipher = {
-    encode (texto, desplazamiento){
-        let resultado= '';
-        for (let i=0; i < texto.length; i++){
-        let mayusculas= texto.toUpperCase();
-        let ascii= mayusculas.charCodeAt(i);
-        let codificando=((ascii - 65 + parseInt(desplazamiento)) %26 + 65);
-        resultado += String.fromCharCode(codificando);
-        
-  }    return resultado;
-  },
- decode (texto, desplazamiento){
-  let resultado2= '';
-    for(let i=0 ;i < texto.length; i++){
+  encode (texto, desplazamiento){
+      let resultado= '';
+      let codificando;
+      for (let i=0; i < texto.length; i++){
       let mayusculas= texto.toUpperCase();
       let ascii= mayusculas.charCodeAt(i);
-      let decodificando=((ascii + 65 - parseInt(desplazamiento)) %26 + 65);
-      resultado2 += String.fromCharCode(decodificando);
-    } return resultado2;
-  }};
+      if (ascii==32) {
+        codificando= 32;
+      } else {
+        codificando=((ascii - 65 + parseInt(desplazamiento)) %26 + 65);
+       }
+  resultado += String.fromCharCode(codificando);      
+}    return resultado;
+},
+decode (texto, desplazamiento){
+let resultado2= '';
+let decodificando;
+  for(let i=0 ;i < texto.length; i++){
+    let mayusculas= texto.toUpperCase();
+    let ascii= mayusculas.charCodeAt(i);
+    if (ascii==32) {
+      decodificando= 32;
+    } else {
+      decodificando=((ascii + 65 - parseInt(desplazamiento)) %26 + 65);
+    }
+    resultado2 += String.fromCharCode(decodificando);  
+  } return resultado2;
+}};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import cipher from "./cipher.js";
 
     //variable que conecta con DOM y recoge id de caja de texto
     document.getElementById("siguiente").addEventListener('click',recogeDatos);
-    document.getElementById("cifrando").addEventListener('click', cifrar);
-    document.getElementById("descifrado").addEventListener('click', descifrar);
+    document.getElementById("cifrando").addEventListener('click',cifrar);
+    document.getElementById("descifrado").addEventListener('click',descifrar);
     document.getElementById("volver").addEventListener('click',volver);
     document.getElementById("salir").addEventListener('click',salir);
-
+    
 //función para recoger datos de nombre de usuario
 function recogeDatos() {
+    let nombre=document.getElementById("cajanombre").value;
      //Condicional para mandar alerta si no hay texto
-    /*if (nombre.length == " "){
+    if (nombre.length == " "){
         document.getElementById("advertencia").innerText = "Por favor poner tu nombre";
     //Condicional para mandar a segunda pantalla y dar bienvenida
-    } else {*/
-        let nombre= document.getElementById("cajanombre").value;
+    } else {
         document.getElementById("pantallaUno").style.display="none";
         document.getElementById("pantallaDos").style.display="block";
         document.getElementById("bienvenido").innerText= "Bienvenid@ " + nombre;
+    }
     }
 
 //Aquí comienza interacción para cifrar/descifrar
@@ -25,15 +26,15 @@ function recogeDatos() {
         let texto= document.getElementById("cajaMensaje").value;
         let desplazamiento= document.getElementById("cajaClave").value;
         let resultadoMensaje= cipher.encode(texto,desplazamiento);
-        document.getElementById("cajaResultado").innerHTML="Tu mensaje cifrado es: " + resultadoMensaje;      
-}
+        document.getElementById("cajaResultado").innerHTML=resultadoMensaje;      
+    }
 
     function descifrar () {
         let texto= document.getElementById("cajaMensaje").value;
         let desplazamiento= document.getElementById("cajaClave").value;
         let resultadoMensaje= cipher.decode(texto,desplazamiento);
-        document.getElementById("cajaResultado").innerHTML="Tu mensaje descifrado es: " +resultadoMensaje; 
-      }
+        document.getElementById("cajaResultado").innerHTML=resultadoMensaje; 
+    }
 
     function volver () {
         document.getElementById("pantallaDos").style.display="none";
