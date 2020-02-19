b3effa3b9a7b8add5d3142edf278b4ed641ffcef
paocmoras <paocmoras@hotmail.com>
2020-02-18 20:05:27 -0600

    pasando todo a un solo html y haciendo anotaciones

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-window.onload = () => {
+    window.onload = () => {
 
-    /*activado y desactivado de los enlaces de cada pestaña*/
+    //variable de mi boton "entendido" con el nombre de id "boton"
+    //nota: se le tiene que poner un div id al comienzo de cada pagina para
+    //poderla meter en eventos y poder pasar a la siguiente pagina
+
+    const boton = document.getElementById("boton");
+
+    boton.addEventListener("click", function() {
+    console.log("Has hecho clic en el botón");
+    document.getElementById("first").style.display = "none";
+    document.getElementById("second").style.display= "block";
+
+    });
+
+
+    /*pantalla cifrado*/
 
     const cifrado = document.getElementById("page-encode");//se liga a href
-    const descifrado = document.getElementById("page-decode");//se liga a href
 
-
-
-
-   cifrado.addEventListener("click", function() {
-
+    cifrado.addEventListener("click", function() {
       document.getElementById("text-encode").style.display = "none";
       document.getElementById("text-encode").style.display= "block";
       document.getElementById("text-decode").style.display= "none";
     });
 
-    descifrado.addEventListener("click", function() {
-
-      document.getElementById("text-decode").style.display = "none";
-      document.getElementById("text-decode").style.display = "block";
-      document.getElementById("text-encode").style.display = "none";
-    });
+    /*variables para funcionamiento de botones cifrar*/
 
     const btnTextEncode = document.getElementById('btn_encode');
     btnTextEncode.addEventListener("click", function() {
@@ -32,12 +36,26 @@ window.onload = () => {
     });
 
 
+    /*pantalla descifrado*/
+
+    const descifrado = document.getElementById("page-decode");//se liga a href
+
+    descifrado.addEventListener("click", function() {
+
+      document.getElementById("text-decode").style.display = "none";
+      document.getElementById("text-decode").style.display = "block";
+      document.getElementById("text-encode").style.display = "none";
+    });
+
+    /*variables para funcionamiento de botones descifrar*/
+
     const btnTextDecode = document.getElementById("btn_decode");
     btnTextDecode.addEventListener("click", function() {
-        //enlaza a caja de texto html y css
-        let textDecode = document.getElementById("box-msg-decode").value;
-        //Enlaza offset a html y css
-        let number_offset = document.getElementById("offset_des").value;
-        document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, number_offset);
+      //enlaza a caja de texto html y css
+      let textDecode = document.getElementById("box-msg-decode").value;
+      //Enlaza offset a html y css
+      let number_offset = document.getElementById("offset_des").value;
+      document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, number_offset);
     });
+
 }
