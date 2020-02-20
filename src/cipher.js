f3e92adae570fce43a1e633748655bd83609ec5d
AniaCuapio <ania.cuapio@gmail.com>
2020-02-20 13:35:34 -0600

    Ultimos cambios al css

--- src/cipher.js
+++ src/cipher.js
@@ -4,17 +4,17 @@ const cipher = {
 export default cipher;
 //ENCODE
 //function encode(offset,str) {
-//  if (typeof offset !=="number" && typeof str !=="string") {
+//if (typeof offset !=="number" && typeof str !=="string") {
 //    throw new TypeError
 //  } else {
 //  let solved="";
 //  for (let i=0; i<str.length; i++) {          //Esta parte le dice al bucle que debe repetirse hasta llegar a la última letra.
-//      let asciiNum = str.charCodeAt(i);           //Esto genera el códigos ascii de cada letra
-//      if (asciiNum < 65 || asciiNum > 90){
-//      alert("Ingresa sólo mayúsculas.")
-//      throw new TypeError;}
-//      else {
-//        asciiNum = (asciiNum-65+parseInt(offset))%26+65;//Esto genera el número por el que se debe desplazar el alfabeto y lo suma a la posicion original de cada letra
+//  let asciiNum = str.charCodeAt(i);           //Esto genera el códigos ascii de cada letra
+//  if (asciiNum < 65 || asciiNum > 90){
+//  alert("Ingresa sólo mayúsculas.")
+//  throw new TypeError;}
+//  else {
+//    asciiNum = (asciiNum-65+parseInt(offset))%26+65;//Esto genera el número por el que se debe desplazar el alfabeto y lo suma a la posicion original de cada letra
 //      solved += String.fromCharCode(asciiNum);  //Esta parte llama a la nueva letra a partir de los nuevos codigos ascii de las nuevas posiciones.
 //    }
 //  }
@@ -22,7 +22,7 @@ export default cipher;
 //  }
 //}
 
-//TO DO: ENCODE con soporte para todos los caracteres imprimibles
+//ENCODE con soporte para todos los caracteres imprimibles
 function encode(offset,str) {
   if (typeof offset !=="number" && typeof str !=="string") {
     throw new TypeError
@@ -37,7 +37,7 @@ function encode(offset,str) {
 }
 }
 
-//function decode(offset,str) {
+//function decode(offset, str) {
 //  if (typeof offset !=="number" && typeof str !=="string") {
 //    throw new TypeError
 //  } else {
@@ -46,28 +46,26 @@ function encode(offset,str) {
 //      let asciiNum = str.charCodeAt(i);           //Esto genera el códigos ascii de cada letra
 //      if (asciiNum < 65 || asciiNum > 90){
 //      throw new TypeError}
-//      else {
+//     else {
 //        asciiNum = (asciiNum+65-parseInt(offset))%26+65;//Esto genera el número por el que se debe desplazar el alfabeto y lo suma a la posicion original de cada letra
 //      solved += String.fromCharCode(asciiNum);  //Esta parte llama a la nueva letra a partir de los nuevos codigos ascii de las nuevas posiciones.
 //    }
 //  }
+//}
 //    return(solved);
-//  }
 //}
 
-
-//TO DO: decode con soporte para todos los caracteres imprimibles
+//DECODE con soporte para todos los caracteres imprimibles
 function decode(offset,str) {
   if (typeof offset !=="number" && typeof str !=="string") {
     throw new TypeError
   } else {
-    let solved="";
-  for (let i=0; i<str.length; i++) {          //Esta parte le dice al bucle que debe repetirse hasta llegar a la última letra.
+    let solved;
+    for (let i=0; i<str.length; i++) {          //Esta parte le dice al bucle que debe repetirse hasta llegar a la última letra.
       let asciiNum = str.charCodeAt(i);           //Esto genera el códigos ascii de cada letra
        asciiNum = (asciiNum-parseInt(offset))%255;//Esto genera el número por el que se debe desplazar el alfabeto y lo suma a la posicion original de cada letra
       solved += String.fromCharCode(asciiNum);  //Esta parte llama a la nueva letra a partir de los nuevos codigos ascii de las nuevas posiciones.
     }
-//  }
+  }
     return(solved);
-}
 }
