3f30c50c57d11da469bfbb2626ab87b0c52bce8c
AILH13 <naitlaghas@gmail.com>
2020-02-20 09:24:09 -0600

    Cambio a index.html, index.js y style.css intentando cambio pantallas pero sin agregar botones.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
+// Variables
+// Variables pantallas
+let firstScreen = document.getElementById("inicio-menu");
+let secondScreen = document.getElementById("aportaciones");
+let thirdScreen = document.getElementById("elegir-aport");
+let quarterScreen = document.getElementById("alert-confirm-aport");
+let fifthScreen = document.getElementById("cupones-disp");
+let sixthScreen = document.getElementById("alert-confirm-cupon");
+
+// Funciones para el cambio de pantallas
+function segundaPantalla() {
+    firstScreen.style="display:none";
+    secondScreen.style="display:block";
+    thirdScreen.style="display:none";
+}
+
+function terceraPantalla() {
+    firstScreen.style="display:none";
+    secondScreen.style="display:none";
+    thirdScreen.style="display:block";
+} 
+
 console.log(cipher);
