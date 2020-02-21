f1fe158743f81b420e98653b45e404fd0944b97f
fabi-pharoxysme <fabio.ola9859@gmail.com>
2020-02-21 09:28:40 -0600

    ultimos cambios que realice

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,37 @@
-const cipher = {
-  // ...
-};
+//objeto cipher para poder nombrarlo en index.js
+const cipher = { encode,decode};
 
-export default cipher;
+  export default cipher;
+//Codificar
+  function encode (string, offset) {
+   let solved = ""; //Donde se va a imprimir el resultado
+   let convertAscii; //Se almacena el codigo ASCII
+   let newLetter; //Se alamacena "las nuevas letras" que se formaron con el codigo ASCII y el offset ya aplicado
+   for(let i = 0; i < string.length; i++){
+    let convertAscii= string.charCodeAt(i);
+    if(convertAscii >= 97 &&convertAscii <=122){ //Si son minusculas, devuelve un error
+      throw new TypeofError
+    }else if (convertAscii >= 65 && convertAscii <= 90){
+      let newLetter = (convertAscii-65+parseInt(offset))%26+65;
+     solved += String.fromCharCode(newLetter);
+    }
+   }
+   return(solved);
+   }
+
+   //Decodificar
+  
+   function decode(string,offset){
+   let solved="";
+   let convertAscii;
+   let newLetter;
+   for (let i = 0; i < string.length; i++){
+     if (convertAscii >= 97 && convertAscii <=122){
+       throw new TypeError       
+     }else if (convertAscii >65 && convertAscii <=90){
+       let newLetter = (convertAscii-65+parseInt(offset))%26+65;
+       solved += String.fromCharCode(newLetter);
+      }
+   }
+   return(solved);
+  }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
+let string = document.getElementById("name").value;
+let offset = string.length;
 
 console.log(cipher);
+console.log(cipher.encode);
+console.log(cipher.decode);
+
+function changePag(){ // Cambio de pantalla
+  let page= document.getElementById("principalPage")
+ page.style = "display:none"
+ document.getElementById("print").style.display = "block";
+}
+document.getElementById("siguiente").addEventListener ('click',changePag);
+
+function encode (){
+  let textEncode = (cipher.encode(string,offset))
+  document.getElementById("cipherName").innerHTML= textEncode;
+}
+document.getElementById("siguiente").addEventListener("click", encode);
+
+function printExpierence(){
+  let experience = document.getElementById("textarea").value
+ document.getElementById("textareaPrint").innerText = experience;
+}
+document.getElementById("siguiente").addEventListener('click', printExpierence)
