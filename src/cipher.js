77e605b6a66287a6b54c86fd812f415b2313263d
Ana Rodea <60738950+AnaRodea@users.noreply.github.com>
2020-02-21 08:35:52 -0600

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
-const cipher = {
+//este archivo es para completar las pruebas unitarias
+
+
+const cipher = {encode,decode
   // ...
 };
+
+
+function encode (offset, world){
+//definir variables; 1,. nueva posición , e
+
+}
+
+
+
+
+
+
+
 
 export default cipher;
